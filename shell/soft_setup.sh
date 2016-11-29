e53b3930db5f183da05b3347cf968d1302aefb81
liguoqing <liguoqing@lianjia.com>
2016-11-29 17:35:36 +0800

    shell

--- shell/soft_setup.sh
+++ shell/soft_setup.sh
@@ -40,7 +40,6 @@ setup_jdk()
     rm -f ${TMP_HOME}/${JDK_VERSION}.tar.gz
     rm -rf ${JDK_DIR}
     wget ${URL}/soft/${JDK_VERSION}.tar.gz -O ${TMP_HOME}/${JDK_VERSION}.tar.gz
-    echo "yes" | ${TMP_HOME}/${JDK_VERSION}.tar.gz | cat
     tar -zxvf ${TMP_HOME}/${JDK_VERSION}.tar.gz -C ${JDK_DIR}
 	echo 'profile' ${APP_HOME}/${JDK_DIR}
 }

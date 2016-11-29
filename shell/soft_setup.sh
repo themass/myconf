4ad6e3e12dd63c8c085393782b387a89799c2316
liguoqing <liguoqing@lianjia.com>
2016-11-29 18:01:27 +0800

    shell

--- shell/soft_setup.sh
+++ shell/soft_setup.sh
@@ -87,7 +87,7 @@ setup_sdk()
 	mv ${SDK_PLATFORM_P_VERSION_P_3} ${SDK_VERSION}/platforms/${SDK_PLATFORM_P_VERSION_P_P_3}
 	mv ${SDK_PLATFORM_P_VERSION_P_4} ${SDK_VERSION}/platforms/${SDK_PLATFORM_P_VERSION_P_P_4}
 	
-	echo 'profile' ${APP_HOME}/${SDK_VERSION}
+	echo 'profile' ${APP_HOME}/${SDK_VERSION} 
 	
 }
 setup_ndk() 	
@@ -185,9 +185,9 @@ setup_nginx() {
 	--add-module=../${NGINX_CONSISTENT_VERSION} --add-module=../${NGINX_ECHO_VERSION}
 	make -j4
     make install
-    rm -rf ${APP_HOME}/nginx
-    ln -s ${APP_HOME}/${NGINX_VERSION} ${APP_HOME}/nginx
-    rm -rf ${APP_HOME}/nginx/html
+#    rm -rf ${APP_HOME}/nginx
+#    ln -s ${APP_HOME}/${NGINX_VERSION} ${APP_HOME}/nginx
+#    rm -rf ${APP_HOME}/nginx/html
 }
 ## -----------------------
 ## Show help message

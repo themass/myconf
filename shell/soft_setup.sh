c25f3355d7d14c12d615b7c28d3ec9702af9a49f
liguoqing <liguoqing@lianjia.com>
2016-11-29 15:46:41 +0800

    shell

---
+++ shell/soft_setup.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+## -----------------------
+## Version setting
+## -----------------------
+URL=http://61.135.251.70:9876
+APP_HOME=/root/local
+TMP_HOME=/root/soft
+JDK_VERSION=jdk-8u111-linux-x64
+JDK_DIR=jdk1.8
+
+SDK_VERSION=android-sdk_r24.0.2-linux
+SDK_PLATFORM_VERSION=platform-tools_r23.1.0-linux
+SDK_PLATFORM_P_VERSION_1=android-17_r01
+SDK_PLATFORM_P_VERSION_2=android-19_r03
+SDK_PLATFORM_P_VERSION_3=android-21_r01
+SDK_PLATFORM_P_VERSION_4=android-22_r02
+
+NDK_VERSION= android-ndk64-r10b-linux-x86_64
+
+MAVEN_VERSION=apache-maven-3.0.3
+
+NGINX_VERSION=nginx-1.10.1
+NGINX_REDIS_VERSION=ngx_http_redis-0.3.7
+NGINX_REDIS2_VERSION=redis2-nginx-module-0.11
+NGINX_SRCACHE_VERSION=srcache-nginx-module-0.26
+NGINX_DEVEL_VERSION=ngx_devel_kit-0.2.19
+NGINX_MISC_VERSION=set-misc-nginx-module-0.24
+NGINX_SLOWFS_VERSION=ngx_slowfs_cache-1.10
+NGINX_PURGE_VERSION=ngx_cache_purge
+NGINX_PHP_VERSION=ngx_http_php_session-0.3
+NGINX_ACCOUNTING_VERSION=ngx_http_accounting_module-0.2
+NGINX_CONSISTENT_VERSION=ngx_http_consistent_hash
+NGINX_ECHO_VERSION=echo-nginx-module
+NGINX_PCRE_VERSION=pcre-8.39
+
+mkdir -p ${APP_HOME}
+mkdir -p ${TMP_HOME}
+
+#init soft
+setup_jdk() 
+{
+	cd ${APP_HOME}
+    rm -f ${TMP_HOME}/${JDK_VERSION}.tar.gz
+    rm -rf ${JDK_DIR}
+    wget ${URL}/soft/${JDK_VERSION}.tar.gz -O ${TMP_HOME}/${JDK_VERSION}.tar.gz
+    echo "yes" | ${TMP_HOME}/${JDK_VERSION}.bin | cat
+    tar -zxvf ${TMP_HOME}/${JDK_VERSION}.tar.gz
+    mv ${TMP_HOME}/${JDK_VERSION} ${JDK_DIR}
+	echo 'profile' ${APP_HOME}/${JDK_DIR}
+}
+setup_sdk() 
+{
+	cd ${APP_HOME}
+	rm -f ${TMP_HOME}/${SDK_VERSION}.tgz
+	rm -rf ${SDK_VERSION}
+	wget ${URL}/soft/${SDK_VERSION}.tgz -O ${TMP_HOME}/${SDK_VERSION}.tgz
+	tar -zxvf ${TMP_HOME}/${SDK_VERSION}.tgz
+	mv ${TMP_HOME}/${SDK_VERSION} .
+	rm -rf ${TMP_HOME}/${SDK_PLATFORM_VERSION}.zip 
+	wget ${URL}/soft/${SDK_PLATFORM_VERSION}.zip -O ${TMP_HOME}/${SDK_PLATFORM_VERSION}.zip
+	unzip ${TMP_HOME}/${SDK_PLATFORM_VERSION}.zip
+	mv ${TMP_HOME}/platform-tools  ${SDK_VERSION}/
+	
+	rm -f ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_1}.zip
+	rm -f ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_2}.zip
+	rm -f ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_3}.zip
+	rm -f ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_4}.zip
+	
+	wget ${URL}/soft/${SDK_PLATFORM_P_VERSION_1}.zip -O ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_1}.zip
+	wget ${URL}/soft/${SDK_PLATFORM_P_VERSION_2}.zip -O ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_2}.zip
+	wget ${URL}/soft/${SDK_PLATFORM_P_VERSION_3}.zip -O ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_3}.zip
+	wget ${URL}/soft/${SDK_PLATFORM_P_VERSION_4}.zip -O ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_4}.zip
+	
+	unzip ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_1}.zip  
+	unzip ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_2}.zip  
+	unzip ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_3}.zip  
+	unzip ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_4}.zip  
+	mv ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_1} ${SDK_VERSION}/platforms/
+	mv ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_2} ${SDK_VERSION}/platforms/
+	mv ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_3} ${SDK_VERSION}/platforms/
+	mv ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_4} ${SDK_VERSION}/platforms/
+	
+	echo 'profile' ${APP_HOME}/${SDK_VERSION}
+	
+}
+setup_ndk() 
+{
+	cd ${APP_HOME}
+	rm -f ${TMP_HOME}/${NDK_VERSION}.tgz
+	rm -rf ${NDK_VERSION}
+	wget ${URL}/soft/${NDK_VERSION}.tar.bz2 -O ${TMP_HOME}/${NDK_VERSION}.tar.bz2
+	tar -jxvf ${TMP_HOME}/${NDK_VERSION}.tar.bz2
+	mv ${TMP_HOME}/${NDK_VERSION} .
+	echo 'profile' ${APP_HOME}/${NDK_VERSION}
+}
+setup_nginx() {
+	cd ${TMP_HOME}
+    rm -f ${NGINX_VERSION}.tar.gz
+    rm -rf ${NGINX_VERSION}
+    rm -rf ${APP_HOME}/${NGINX_VERSION}
+    wget ${URL}/soft/nginx/${NGINX_VERSION}.tar.gz
+    tar -zxvf ${NGINX_VERSION}.tar.gz
+
+	rm -f ${NGINX_REDIS_VERSION}.tar.gz
+    rm -rf ${NGINX_REDIS_VERSION}
+    wget ${URL}/soft/nginx/${NGINX_REDIS_VERSION}.tar.gz
+    tar zxvf ${NGINX_REDIS_VERSION}.tar.gz
+    
+    rm -f ${NGINX_REDIS2_VERSION}.tar.gz
+    rm -rf ${NGINX_REDIS2_VERSION}
+    wget ${URL}/soft/nginx/${NGINX_REDIS2_VERSION}.tar.gz
+    tar zxvf ${NGINX_REDIS2_VERSION}.tar.gz
+    
+    rm -f ${NGINX_SRCACHE_VERSION}.tar.gz
+    rm -rf ${NGINX_SRCACHE_VERSION}
+    wget ${URL}/soft/nginx/${NGINX_SRCACHE_VERSION}.tar.gz
+    tar zxvf ${NGINX_SRCACHE_VERSION}.tar.gz
+    
+    rm -f ${NGINX_DEVEL_VERSION}.tar.gz
+    rm -rf ${NGINX_DEVEL_VERSION}
+    wget ${URL}/soft/nginx/${NGINX_DEVEL_VERSION}.tar.gz
+    tar zxvf ${NGINX_DEVEL_VERSION}.tar.gz
+    
+    rm -f ${NGINX_MISC_VERSION}.tar.gz
+    rm -rf ${NGINX_MISC_VERSION}
+    wget ${URL}/soft/nginx/${NGINX_MISC_VERSION}.tar.gz
+    tar zxvf ${NGINX_MISC_VERSION}.tar.gz
+    
+    rm -f ${NGINX_SLOWFS_VERSION}.tar.gz
+    rm -rf ${NGINX_SLOWFS_VERSION}
+    wget ${URL}/soft/nginx/${NGINX_SLOWFS_VERSION}.tar.gz
+    tar zxvf ${NGINX_SLOWFS_VERSION}.tar.gz
+    
+    rm -f ${NGINX_PURGE_VERSION}.tar.gz
+    rm -rf ${NGINX_PURGE_VERSION}
+    https://github.com/FRiCKLE/ngx_cache_purge.git
+    
+    
+    rm -f ${NGINX_PHP_VERSION}.tar.gz
+    rm -rf ${NGINX_PHP_VERSION}
+    wget ${URL}/soft/nginx/${NGINX_PHP_VERSION}.tar.gz
+    tar zxvf ${NGINX_PHP_VERSION}.tar.gz
+    
+    rm -f ${NGINX_ACCOUNTING_VERSION}.tar.gz
+    rm -rf ${NGINX_ACCOUNTING_VERSION}
+    wget ${URL}/soft/nginx/${NGINX_ACCOUNTING_VERSION}.tar.gz
+    tar zxvf ${NGINX_ACCOUNTING_VERSION}.tar.gz
+    
+    rm -f ${NGINX_PCRE_VERSION}.tar.gz
+    rm -rf ${NGINX_PCRE_VERSION}
+    wget ${URL}/soft/nginx/${NGINX_PCRE_VERSION}.tar.gz
+    tar -zxvf ${NGINX_PCRE_VERSION}.tar.gz
+    
+    rm -rf ${NGINX_ECHO_VERSION}
+    git clone git://github.com/agentzh/echo-nginx-module.git
+
+    rm -rf ${NGINX_CONSISTENT_HASH_VERSION}
+    git clone https://github.com/replay/ngx_http_consistent_hash.git
+
+	
+    cd ${NGINX_VERSION}
+	./configure --prefix=${APP_HOME}/${NGINX_VERSION} --with-http_stub_status_module --with-http_ssl_module --with-http_gzip_static_module \
+	--with-pcre=../${NGINX_PCRE_VERSION}
+	--add-module=../${NGINX_REDIS_VERSION} --add-module=../${NGINX_REDIS2_VERSION} --add-module=../${NGINX_SRCACHE_VERSION} \
+	--add-module=../${NGINX_DEVEL_VERSION} --add-module=../${NGINX_PURGE_VERSION} --add-module=../${NGINX_SLOWFS_VERSION=} \
+	--add-module=../${NGINX_PHP_VERSION} --add-module=../${NGINX_ACCOUNTING_VERSION} --add-module=../${NGINX_CONSISTENT_VERSION}  \
+	--add-module=../${NGINX_CONSISTENT_VERSION} --add-module=../${NGINX_ECHO_VERSION}
+	make -j4
+    make install
+    rm -rf ${APP_HOME}/nginx
+    ln -s ${APP_HOME}/${NGINX_VERSION} ${APP_HOME}/nginx
+    rm -rf ${APP_HOME}/nginx/html
+}
+## -----------------------
+## Show help message
+## -----------------------
+usage() 
+{
+    echo "Available arguments as below:"
+    echo "jdk           Setup init jdk"
+    echo "sdk          Setup sdk"
+    echo "ndk          Setup ndk"
+    echo "nginx          Setup nginx"
+    echo "all           Setup all aboves"
+}
+
+## =====================================
+## The main process
+## =====================================
+if [ $# != 0 ]; then
+    for arg in $*; do
+        case "$arg" in
+            jdk)            setup_jdk;;
+            sdk)          setup_sdk;;
+			ndk)          setup_ndk;;
+			nginx)          setup_nginx;;
+			all)          setup_all;;
+        esac
+    done
+else
+    usage
+fi
---
+++ shell/vpn_setup.sh
@@ -0,0 +1,131 @@
+#init soft
+ip=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|grep -v 10. |awk '{print $2}'|tr -d "addr:"`
+init_soft() 
+{
+	apt-get update
+	apt-get install build-essential lrzsz  tree dstat git dos2unix unzip	#编译环境
+	aptitude install libgmp10 libgmp3-dev libssl-dev pkg-config libpcsclite-dev libpam0g-dev     #编译所需要的软件
+}
+#strongswan setup
+strongswan_setup() 
+{
+	wget http://download.strongswan.org/strongswan-5.2.2.tar.bz2
+	tar -jxvf strongswan-5.2.2.tar.bz2 && cd strongswan-5.2.2
+	./configure --prefix=/usr --sysconfdir=/etc  --enable-openssl --enable-nat-transport --disable-mysql --disable-ldap  --disable-static --enable-shared --enable-md4 --enable-eap-mschapv2 --enable-eap-aka --enable-eap-aka-3gpp2  --enable-eap-gtc --enable-eap-identity --enable-eap-md5 --enable-eap-peap --enable-eap-radius --enable-eap-sim --enable-eap-sim-file --enable-eap-simaka-pseudonym --enable-eap-simaka-reauth --enable-eap-simaka-sql --enable-eap-tls --enable-eap-tnc --enable-eap-ttls
+	make && make install
+	ipsec start 
+	cd ..
+}
+strongswan_config() 
+{
+	unzip strongswan_conf.zip
+	dos2unix strongswan_conf/ipsec.conf
+	dos2unix strongswan_conf/charon.conf
+	dos2unix strongswan_conf/ipsec.secrets
+	
+	cp  /etc/ipsec.conf /etc/ipsec.conf.bak
+	cp  strongswan_conf/ipsec.conf /etc/ipsec.conf
+
+	cp  /etc/strongswan.d/charon.conf /etc/strongswan.d/charon.conf.bak
+	cp  strongswan_conf/charon.conf /etc/strongswan.d/charon.conf
+
+	cp  /etc/ipsec.secrets  /etc/ipsec.secrets.bak
+	cp  strongswan_conf/ipsec.secrets /etc/ipsec.secrets
+	ipsec restart
+}
+#ca setup
+init_ca() 
+{
+	mkdir -p ca
+	cd ca
+	echo "C=CN, O=timeline, CN=${ip}"
+	ipsec pki --gen --outform pem > caKey.pem
+	ipsec pki --self --in caKey.pem --dn "C=CN, O=timeline, CN=${ip}" --ca --outform pem > caCert.pem
+
+	ipsec pki --gen --outform pem > serverKey.pem
+	ipsec pki --pub --in serverKey.pem | ipsec pki --issue --cacert caCert.pem --cakey caKey.pem --dn "C=CN, O=timeline, CN=${ip}" --san="${ip}" --flag serverAuth --flag ikeIntermediate --outform pem > serverCert.pem
+
+	ipsec pki --gen --outform pem > clientKey.pem
+	ipsec pki --pub --in clientKey.pem | ipsec pki --issue --cacert caCert.pem --cakey caKey.pem --dn "C=CN, O=timeline, CN=client" --outform pem > clientCert.pem
+
+	openssl pkcs12 -export -inkey clientKey.pem -in clientCert.pem -name "client" -certfile caCert.pem -caname "${ip}" -out clientCert.p12
+
+	cp caCert.pem /etc/ipsec.d/cacerts/
+	cp serverCert.pem /etc/ipsec.d/certs/
+	cp serverKey.pem /etc/ipsec.d/private/
+	cp clientCert.pem /etc/ipsec.d/certs/
+	cp clientKey.pem /etc/ipsec.d/private/
+	ipsec restart
+}
+
+
+# iptables
+setup_iptables() 
+{
+	iptables -A INPUT -p udp --dport 500 -j ACCEPT 
+	iptables -A INPUT -p udp --dport 4500 -j ACCEPT 
+	iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE 
+	iptables -A FORWARD -s 10.0.0.0/24 -j ACCEPT 
+	iptables -A FORWARD -d 10.0.0.0/24 -j ACCEPT
+	ip6tables -A INPUT -p udp --dport 4500 -m frag --fragfirst -j CONNMARK --set-mark 0x42
+	ip6tables -A INPUT -p udp --dport 4500 -j ACCEPT
+	ip6tables -A INPUT -m frag -m connmark --mark 0x42 -j ACCEPT
+	#为避免VPS重启后NAT功能失效，可以把如上5行命令添加到 /etc/rc.local 文件中，添加在exit那一行之前即可。
+	#service iptables save
+	#service iptables restart
+	#systemctl restart iptables
+	iptables-save
+	#iptables-restore
+}
+#net
+# vi /etc/sysctl.conf
+net() 
+{
+	net.ipv4.ip_forward = 1
+	net.ipv6.conf.all.forwarding=1
+	sysctl -p
+}
+## -----------------------
+## Setup all aboves
+## -----------------------
+setup_all() 
+{
+    init_soft
+    strongswan_setup
+    strongswan_config
+    init_ca	
+    setup_iptables   
+}
+## -----------------------
+## Show help message
+## -----------------------
+usage() 
+{
+    echo "Available arguments as below:"
+    echo "soft           Setup init soft"
+    echo "strongswan          Setup strongswan"
+    echo "strongswanconf          Setup strongswan config"
+    echo "ca           Setup ca"
+    echo "iptables         Setup iptables"
+    echo "net    Setup net"
+    echo "all           Setup all aboves"
+}
+
+## =====================================
+## The main process
+## =====================================
+if [ $# != 0 ]; then
+    for arg in $*; do
+        case "$arg" in
+            soft)            init_soft;;
+            strongswan)          strongswan_setup;;
+	    ca)          init_ca;;
+	    iptables)          setup_iptables;;
+	    strongswanconf)          strongswan_config;;
+	    net)          net;;
+	    all)          setup_all;;
+        esac
+    done
+else
+    usage
+fi

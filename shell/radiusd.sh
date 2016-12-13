9cc9c70265b258d618f0029ed2e7499ce7d52f30
liguoqing <liguoqing@lianjia.com>
2016-12-13 18:53:16 +0800

    11

---
+++ shell/radiusd.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+## -----------------------
+## Version setting
+## -----------------------
+
+restart() 
+{
+	pkill -9 radiusd
+	radiusd &
+}
+stop() 
+{
+	pkill -9 radiusd
+}
+debug() 
+{
+	pkill -9 radiusd
+	radiusd -X
+}
+usage() 
+{
+    echo "Available arguments as below:"
+    echo "start           start"
+    echo "stop          stop"
+    echo "debug          debug"
+}
+
+## =====================================
+## The main process
+## =====================================
+if [ $# != 0 ]; then
+    for arg in $*; do
+        case "$arg" in
+            start)            restart;;
+            stop)          stop;;
+            debug)          debug;;
+        esac
+    done
+else
+    usage
+fi

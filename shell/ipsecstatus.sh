99c090c505c2512a22d6c919f98279da5c031cdf
liguoqing <liguoqing@lianjia.com>
2017-10-25 23:02:52 +0800

    11

--- shell/ipsecstatus.sh
+++ shell/ipsecstatus.sh
@@ -18,7 +18,7 @@ restart()
 status_send() 
 {
 	c=`/usr/sbin/ipsec status | grep Associations | awk -F '(' '{print$2}' |awk -F' up' '{print$1}'`
-	local_host = `/bin/hostname`
+	local_host=`/bin/hostname`
 	 if [ -n "${c}" ]; then
 	 	curl  -d "count=${c} localhost=${local_host}" http://api.sspacee.com/vpn/api/noapp/collect.json
 	 	echo '${local_host}'

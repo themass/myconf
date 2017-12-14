ce4161e71e7c37b0138028178a50980525dea63c
liguoqing <liguoqing@lianjia.com>
2017-12-14 20:48:59 +0800

    11

--- python/monitor/pingport.py
+++ python/monitor/pingport.py
@@ -6,17 +6,18 @@ if __name__ == '__main__':
     dbVPN = db.DbVPN()
     ops = db_ops.DbOps(dbVPN)
     hosts = ops.getAllHost()
-    for i in range(0, 2):
+    for i in range(0, 1):
         for item in hosts:
             cmd = 'nc -u -n -v  %s -z %s ' % (item['gateway'],
                                               item['port'])
-            textlist = os.popen(cmd).readlines()
-            print textlist
-            text = ''
-            for line in textlist:
-                text = text + line
-            print text
-            if text.find('succeeded') > 0:
-                pass
-            else:
-                print item['gateway'], '---连不上，请检查'
+            print '---test ip--%s', item['gateway']
+            textlist = os.popen(cmd).re
+#             print textlist
+#             text = ''
+#             for line in textlist:
+#                 text = text + line
+#             print text
+#             if text.find('succeeded') > 0:
+#                 pass
+#             else:
+#                 print item['gateway'], '---连不上，请检查'

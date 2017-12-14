a764afb217095eaf4eeac4a470023cacf0c2ea71
liguoqing <liguoqing@lianjia.com>
2017-12-15 00:16:04 +0800

    11

--- python/monitor/pingport.py
+++ python/monitor/pingport.py
@@ -15,18 +15,17 @@ if __name__ == '__main__':
     dbVPN = db.DbVPN()
     ops = db_ops.DbOps(dbVPN)
     hosts = ops.getAllHost()
-    for i in range(0, 1):
-        for item in hosts:
-            cmd = 'nc -u -n -v  %s -z %s ' % (item['gateway'],
-                                              item['port'])
-            print '---test ip--%s', item['gateway']
-            os.popen(cmd)
+#     for i in range(0, 1):
+#         for item in hosts:
+#             cmd = 'nc -u -n -v  %s -z %s ' % (item['gateway'],
+#                                               item['port'])
+#             print '---test ip--%s', item['gateway']
+#             os.popen(cmd)
     for item in hosts:
-        cmd = 'ping  -c 10 %s' % (item['gateway'])
+        cmd = 'ping  -c 5 %s' % (item['gateway'])
         lines = os.popen(cmd).readlines()
         count = 0
-        for line in lines:
-
+        for line in lines[1:len(lines)]:
             num = int(parse(line))
             count = count + num
-        print 'ip: %s --------cost %s' % (item['gateway'], count / 10)
+        print 'ip: %s --------cost %s' % (item['gateway'], count / 5)

40b3480c8cd869f077a45754fdd80ccd1c3739c8
liguoqing <liguoqing@lianjia.com>
2017-12-15 12:49:37 +0800

    11

--- python/monitor/pingport.py
+++ python/monitor/pingport.py
@@ -10,6 +10,21 @@ reg = re.compile(
     r"rtt min/avg/max/mdev = ([0-9\.]+)/([0-9\.]+)/([0-9\.]+)")
 
 
+def myAlign(string, length=0):
+    if length == 0:
+        return string
+    slen = len(string)
+    re = string
+    if isinstance(string, str):
+        placeholder = ' '
+    else:
+        placeholder = u'　'
+    while slen < length:
+        re += placeholder
+        slen += 1
+    return re
+
+
 def parse(pingtexts):
     for line in pingtexts:
         if line.count("rtt min") > 0:
@@ -29,10 +44,10 @@ if __name__ == '__main__':
     #             os.popen(cmd)
     for item in hosts:
         if item['enable'] == 0:
-            print '公司：【%-15s】----国家：【%-15s】------ip: 【%-15s】 --------不可用' % (item['com'], item['cname'], item['gateway'])
+            print '公司：【%-15s】----国家：【%s】------ip: 【%-15s】 --------不可用' % (item['com'], myAlign(item['cname'], 10), item['gateway'])
             continue
 
         cmd = 'ping  -c2 -w2 %s' % (item['gateway'])
         lines = os.popen(cmd).readlines()
         num = parse(lines)
-        print '公司：【%-15s】----国家：【%-10s】------ip: 【%-15s】 --------cost:【%-15s】' % (item['com'], item['cname'], item['gateway'], num)
+        print '公司：【%-15s】----国家：【%s】------ip: 【%-15s】 --------cost:【%-15s】' % (item['com'], myAlign(item['cname'], 10), item['gateway'], num)

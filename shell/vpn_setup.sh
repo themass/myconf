3e8291a680ba3980c727ca157d18ecb8a72317a1
liguoqing <liguoqing@lianjia.com>
2017-12-19 14:07:55 +0800

    11

--- shell/vpn_setup.sh
+++ shell/vpn_setup.sh
@@ -169,10 +169,10 @@ setup_all()
     init_soft
     strongswan_setup
     strongswan_config
-    get_ip
-    init_ca	$s
-    get_netdev
-    setup_iptables $s
+    ip=$(get_ip)
+    init_ca	$ip
+    dev=$(get_netdev)
+    setup_iptables $dev
     setup_fail2ban
     net
 }
@@ -180,7 +180,8 @@ get_ip(){
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
-    [ ! -z ${IP} ] && echo ${IP} || echo
+    [ ! -z ${IP} ] && echo ${IP} 
+    echo ${IP}
 }
 get_netdev(){
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )

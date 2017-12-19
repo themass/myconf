5c623d4401d9cb7805bbae76ad173530be109a62
liguoqing <liguoqing@lianjia.com>
2017-12-19 14:00:10 +0800

    11

--- shell/vpn_setup_test.sh
+++ shell/vpn_setup_test.sh
@@ -63,10 +63,10 @@ setup_all()
     init_soft
     strongswan_setup
     strongswan_config
-    get_ip
-    init_ca	$s
-    get_netdev
-    setup_iptables $s
+    ip=$(get_ip)
+    init_ca	$ip
+    dev=$(get_netdev)
+    setup_iptables $dev
 }
 get_ip()
 {
@@ -74,12 +74,13 @@ get_ip()
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
     [ ! -z ${IP} ] && echo ${IP} || echo
+    return ${IP}
 }
 get_netdev()
 {
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
     DEV=$( ip addr | grep ${IP} ||egrep -o '(eth0|ens3)')
-    echo ${DEV}
+    return ${DEV}
 }
 ## -----------------------
 ## Show help message

747949ceaaf6be5f72730c0292641644e10ad76e
liguoqing <liguoqing@lianjia.com>
2017-12-19 14:06:34 +0800

    11

--- shell/vpn_setup_test.sh
+++ shell/vpn_setup_test.sh
@@ -70,14 +70,14 @@ get_ip()
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
-    [ ! -z ${IP} ] && echo ${IP} || echo
-    return ${IP}
+    [ ! -z ${IP} ] && echo ${IP}
+    echo ${IP}
 }
 get_netdev()
 {
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
     DEV=$( ip addr | grep ${IP} |egrep -o '(eth0|eth1|ens3)')
-    return ${DEV}
+    echo ${DEV}
 }
 ## -----------------------
 ## Show help message

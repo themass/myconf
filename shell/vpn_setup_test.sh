8ad9c0e9cbbc0f13e849e98944bbc4c9b928361f
liguoqing <liguoqing@lianjia.com>
2017-12-19 13:57:07 +0800

    11

--- shell/vpn_setup_test.sh
+++ shell/vpn_setup_test.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+## -----------------------
+## Version setting
+## -----------------------
+
 #init soft
 
 #http://it.zhaozhao.info/archives/41127
@@ -64,13 +69,15 @@ setup_all()
     get_netdev
     setup_iptables $s
 }
-get_ip(){
+get_ip()
+{
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
     [ ! -z ${IP} ] && echo ${IP} || echo
 }
-get_netdev(){
+get_netdev()
+{
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
     DEV=$( ip addr | grep ${IP} ||egrep -o '(eth0|ens3)')
     echo ${DEV}

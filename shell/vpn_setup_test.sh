2e575082fe0838b86a1bbd7653293f154bdcf91b
liguoqing <liguoqing@lianjia.com>
2017-12-19 13:56:11 +0800

    11

---
+++ shell/vpn_setup_test.sh
@@ -0,0 +1,117 @@
+#init soft
+
+#http://it.zhaozhao.info/archives/41127
+#https://segmentfault.com/a/1190000002540601
+#http://www.cnblogs.com/hyzhou/category/336618.html
+WORKDIR=/root/work
+TMP_HOME=/root/soft/temp
+PWD=`pwd`
+#ip=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|grep -v 10. |awk '{print $1}'|tr -d "addr:"`
+init_soft() 
+{
+	
+}
+
+strongswan_setup() 
+{
+	echo 'strongswan_setup'
+}
+strongswan_config() 
+{
+	echo 'strongswan_config'
+}
+#ca setup
+init_ca() 
+{
+	cd ${TMP_HOME}
+	echo '$1'
+	mkdir -p ca
+	cd ca
+	echo "C=CN, O=timeline, CN=$1"
+	ipsec pki --gen --outform pem > caKey.pem
+	ipsec pki --self --in caKey.pem --dn "C=CN, O=timeline, CN=$1" --ca --outform pem > caCert.pem
+
+	ipsec pki --gen --outform pem > serverKey.pem
+	ipsec pki --pub --in serverKey.pem | ipsec pki --issue --cacert caCert.pem --cakey caKey.pem --dn "C=CN, O=timeline, CN=$1" --san="$1" --flag serverAuth --flag ikeIntermediate --outform pem > serverCert.pem
+
+	ipsec pki --gen --outform pem > clientKey.pem
+	ipsec pki --pub --in clientKey.pem | ipsec pki --issue --cacert caCert.pem --cakey caKey.pem --dn "C=CN, O=timeline, CN=client" --outform pem > clientCert.pem
+
+	openssl pkcs12 -export -inkey clientKey.pem -in clientCert.pem -name "client" -certfile caCert.pem -caname "$1" -out clientCert.p12
+
+}
+
+
+# iptables
+setup_iptables() 
+{
+	echo '$1'
+	iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o $1 -j MASQUERADE 
+	iptables-save
+	
+}
+
+## -----------------------
+## Setup all aboves
+## -----------------------
+setup_all() 
+{
+    init_soft
+    strongswan_setup
+    strongswan_config
+    get_ip
+    init_ca	$s
+    get_netdev
+    setup_iptables $s
+}
+get_ip(){
+    local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
+    [ ! -z ${IP} ] && echo ${IP} || echo
+}
+get_netdev(){
+    local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
+    DEV=$( ip addr | grep ${IP} ||egrep -o '(eth0|ens3)')
+    echo ${DEV}
+}
+## -----------------------
+## Show help message
+## -----------------------
+usage() 
+{
+    echo "Available arguments as below:"
+    echo "soft           Setup init soft"
+    echo "strongswan          Setup strongswan"
+    echo "strongswanconf          Setup strongswan config"
+    echo "ca           Setup ca"
+    echo "iptables         Setup iptables"
+    echo "net    Setup net"
+    echo "fail2ban    Setup fail2ban"
+    echo "check    checkspeed"
+     echo "check_vpn    check vpn"
+     echo "net    Setup net"
+    echo "all           Setup all aboves"
+}
+
+## =====================================
+## The main process
+## =====================================
+if [ $# != 0 ]; then
+    for arg in $*; do
+        case "$arg" in
+            soft)            init_soft;;
+            strongswan)          strongswan_setup;;
+	    ca)          init_ca $2;;
+	    iptables)          setup_iptables;;
+	    strongswanconf)          strongswan_config;;
+	    net)          net;;
+	     fail2ban)          setup_fail2ban;;
+	    check)          checkspeed;;
+	    check_vpn)          check_vpn;;
+	    all)          setup_all;;
+        esac
+    done
+else
+    usage
+fi

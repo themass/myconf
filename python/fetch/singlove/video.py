52903bd4b0d24fc39dc574be9c94d948817f3423
liguoqing <liguoqing19861028@163.com>
2017-12-30 03:16:34 +0800

    1111

---
+++ python/fetch/singlove/video.py
@@ -0,0 +1,76 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+from baseparse import *
+from urlparse import urlparse
+from common import common
+
+
+class VideoParse(BaseParse):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        for i in range(1, maxPage):
+            self.videoParse(channel, videoUrl % (i))
+
+    def videoParse(self, channel, url):
+        dataList = []
+        soup = self.fetchUrl(url)
+        divs = soup.findAll("div", {"class": "block"})
+        for div in divs:
+            obj = {}
+            divImg = div.first("div", {"class": "media-image"})
+            if divImg != None:
+                ahref = div.first("a")
+                if ahref != None:
+                    mp4Url = self.parseDomVideo(ahref.get("href"))
+                    if mp4Url == None:
+                        print '没有mp4 文件:', ahref.get("href")
+                        continue
+                    obj['url'] = mp4Url
+                    img = ahref.first("img")
+                    if img != None:
+                        picUrl = baseurl + img.get('data-src')
+                        obj['pic'] = picUrl
+            divTitle = div.first("div", {"class": " block-title"})
+            name = divTitle.first('a').TEXT
+            obj['name'] = name
+            videourl = urlparse(obj['url'])
+            obj['path'] = videourl.path
+            obj['updateTime'] = datetime.datetime.now()
+            obj['channel'] = channel
+            dataList.append(obj)
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        for obj in dataList:
+            ops.inertVideo(obj)
+
+        print 'singlove video --解析完毕 ; channel =', channel, '; len=', len(dataList), url
+        dbVPN.commit()
+        dbVPN.close()
+
+    def parseDomVideo(self, url):
+        header = {'User-Agent':
+                  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": url}
+        try:
+            soup = self.fetchUrl(url, header)
+            iframe = soup.first("iframe")
+            if iframe == None:
+                return None
+            url = "http:%s" % (iframe.get("src"))
+            header = {'User-Agent':
+                      'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": url}
+
+            content = self.fetchContentUrlWithBase(url, header)
+            match = regVideo.search(content)
+            if match == None:
+                return None
+            return match.group(1)
+        except Exception as e:
+            common.format_exception(e)
+            return None
+
+
+def videoParse(queue):
+    queue.put(VideoParse())

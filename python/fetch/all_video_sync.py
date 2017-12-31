7ab6bb34907a96d72005b0bd5dee851b15ea0418
liguoqing <liguoqing19861028@163.com>
2017-12-31 09:46:55 +0800

    111

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -5,13 +5,15 @@ import urllib2
 from common.envmod import *
 import vj69.video
 import ozsese.video
+import ozsese.baseparse
 import singlove.video
 if __name__ == '__main__':
+
+    for channel, url in ozsese.baseparse.channels.items():
+        ozsesevideop = ozsese.video.VideoParse(channel, url)
+        ozsesevideop.run()
     vj69videop = vj69.video.VideoParse()
     vj69videop.run()
-
-    ozsesevideop = ozsese.video.VideoParse()
-    ozsesevideop.run()
 
     singlovevideop = singlove.video.VideoParse()
     singlovevideop.run()

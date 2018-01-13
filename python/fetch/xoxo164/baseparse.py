7553e7e6c8bd0ffcf8088cf0466b8bd22f94de9c
liguoqing <liguoqing19861028@163.com>
2018-01-13 21:24:34 +0800

    11

--- python/fetch/xoxo164/baseparse.py
+++ python/fetch/xoxo164/baseparse.py
@@ -13,7 +13,7 @@ import re
 baseurl = "http://www.xoxo164.com"
 header = {'User-Agent':
           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": baseurl}
-maxPage = 80
+maxPage = 40
 maxCount = 3
 maxPageImg = 10
 regVideo = re.compile(r"mac_url=unescape\('(.*)(http)(.*)'\);")

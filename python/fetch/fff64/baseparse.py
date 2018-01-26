55eacfc07264efe7bfd8d54d7a494ecc8ece76ab
liguoqing <liguoqing19861028@163.com>
2018-01-27 02:21:31 +0800

    111

---
+++ python/fetch/fff64/baseparse.py
@@ -0,0 +1,88 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+import datetime
+import zlib
+import urllib2
+import threading
+from common.envmod import *
+from common import db_ops
+from common import common
+import threading
+from BeautifulSoup import BeautifulSoup
+import re
+# http://www.dehyc.com
+baseurl = "http://www.fff64.com"
+videolUrl='/stli_gthdrdbn/29.html'
+header = {'User-Agent':
+          'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": baseurl}
+maxPage = 15
+maxCount = 3
+videoApi = re.compile(r'http:(.*).m3u8')
+
+
+class BaseParse(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def fetchUrl(self, url, aheader=header):
+        count = 0
+        while count < maxCount:
+            try:
+                req = urllib2.Request(baseurl + url, headers=aheader)
+                content = urllib2.urlopen(req, timeout=300).read()
+                soup = BeautifulSoup(content)
+                return soup
+            except Exception as e:
+                print common.format_exception(e)
+                print '打开页面错误,重试', baseurl + url, '次数', count
+                count = count + 1
+
+        print '打开页面错误,重试3次还是错误', url
+        return BeautifulSoup('')
+    def header(self,name):
+        soup = self.fetchUrl("", None)
+        objs =[]
+        uls = soup.findAll('ul',{'class':'nav_menu clearfix'})
+        for ul in uls:
+            active = ul.first("li",{'class':'active'})
+            if active.text==name:
+                alist = ul.findAll('a')
+                for ahref in alist:
+                    obj ={}
+                    obj['name']=ahref.text
+                    obj['url']=ahref.get('href')
+                    objs.append(obj)
+        return objs
+    def fetchUrlWithBase(self, url, aheader=header):
+        count = 0
+        while count < maxCount:
+            try:
+                req = urllib2.Request(url, headers=aheader)
+                content = urllib2.urlopen(req, timeout=300).read()
+                soup = BeautifulSoup(content)
+                return soup
+            except Exception as e:
+                print common.format_exception(e)
+                print '打开页面错误,重试', url, '次数', count
+                count = count + 1
+
+        print '打开页面错误,重试3次还是错误', url
+        return BeautifulSoup('')
+
+    def fetchContentUrlWithBase(self, url, aheader=header):
+        count = 0
+        while count < maxCount:
+            try:
+                req = urllib2.Request(url, headers=aheader)
+                content = urllib2.urlopen(req, timeout=300).read()
+                return content
+            except Exception as e:
+                print common.format_exception(e)
+                print '打开页面错误,重试', url, '次数', count
+                count = count + 1
+
+        print '打开页面错误,重试3次还是错误', url
+        return ''
+
+    

2e42a4e2f844c434ba705a672b7dfd0db726c917
liguoqing <liguoqing19861028@163.com>
2018-01-31 12:24:45 +0800

    111111111

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -16,38 +16,41 @@ import fff64_vip
 import nomsgus_vip
 import sexx77_vip
 import sp1769_vip
+import x2246_vip
 if __name__ == '__main__':
 
 #webview
-#     for channel, url in ozsese.baseparse.channels.items():
-#         ozsesevideop = ozsese.video.VideoParse(channel, url)
-#         ozsesevideop.run()
-#     vj69videop = vj69.video.VideoParse()
-#     vj69videop.run()
+    for channel, url in ozsese.baseparse.channels.items():
+        ozsesevideop = ozsese.video.VideoParse(channel, url)
+        ozsesevideop.run()
+    vj69videop = vj69.video.VideoParse()
+    vj69videop.run()
 # #     
 # #     
 # #     
 # #     
 # #    #翻墙 
 # #     
-#     weavvideop = weav.video.VideoParse()
-#     weavvideop.run()
+    weavvideop = weav.video.VideoParse()
+    weavvideop.run()
 
 # 
     sp1769_vip.parseVideo()
-#     fff64_vip.parseVideo()
+    fff64_vip.parseVideo()
 # 
-#     se8_vip.parseVideo()
-#     nomsgus_vip.parseVideo()
-#     sexx77_vip.parseVideo()
+    se8_vip.parseVideo()
+    nomsgus_vip.parseVideo()
+    sexx77_vip.parseVideo()
 # # #     
 #  
-#     xoxo164videop = xoxo164.video.VideoParse()
-#     xoxo164videop.run()
+    xoxo164videop = xoxo164.video.VideoParse()
+    xoxo164videop.run()
 # # 
-#     singlovevideop = singlove.video.VideoParse()
-#     singlovevideop.run()
+    singlovevideop = singlove.video.VideoParse()
+    singlovevideop.run()
 # 
-# #     yezmw_vip.parseVideo()
+    yezmw_vip.parseVideo()
     ir6yv = ir6y.video.VideoParse()
     ir6yv.run()
+    
+    x2246_vip.parseVideo()

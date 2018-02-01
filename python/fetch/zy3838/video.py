7229008d2eeda8fe97ec039a1faff3ac7d00c1c1
liguoqing <liguoqing19861028@163.com>
2018-02-01 23:44:13 +0800

    11

---
+++ python/fetch/zy3838/video.py
@@ -0,0 +1,104 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+from baseparse import *
+from urlparse import urlparse
+from common import common
+from urllib import unquote
+import time
+from fetch.profile import *
+
+class VideoParse(BaseParse):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        chs  = self.videoChannel()
+        for ch in chs:
+            ops.inertVideoChannel(ch)
+        print ' channel ok; len=',len(chs)
+        dbVPN.commit()
+        dbVPN.close()
+        for ch in chs:
+            for i in range(1, maxVideoPage):
+                url= ch['url']
+                if i!=1:
+                    url= "%s%s%s"%(ch['url'].replace('1.html',''),i,'.html')
+                self.videoParse(ch['channel'], url)
+                print '解析完成 ', ch['channel'], ' ---', i, '页'
+    def videoChannel(self):
+        objs = []
+        obj={}
+        obj['name']="韩国R级"
+        obj['url']=baseurlR
+        obj['baseurl']=baseurl
+        obj['updateTime']=datetime.datetime.now()
+        obj['pic']=''
+        obj['rate']=1.2
+        obj['channel']=obj['url']
+        obj['showType']=3
+        obj['channelType']='movie'
+        return  objs
+    def videoParse(self, channel, url):
+        dataList = []
+        soup = self.fetchUrl(url)
+        lis = soup.findAll("li", {"class": "p1 m1"})
+        for li in lis:
+            ahref = li.first('a')
+            if ahref!=None:
+                mp4Url  = self.parseDomVideo(ahref.get("href"))
+                if mp4Url==None:
+                    continue
+                if mp4Url.count('.html')!=0 :
+                    print mp4Url,"爱奇艺，忽略"
+                    continue
+                obj = {}
+                obj['url'] = mp4Url
+                img = ahref.first("img")
+                obj['pic'] = img.get('data-original')
+                obj['name'] = ahref.text
+                print obj['name'],mp4Url,obj['pic']
+
+                videourl = urlparse(obj['url'])
+                obj['path'] = videourl.path
+                obj['updateTime'] = datetime.datetime.now()
+                obj['channel'] = channel
+                dataList.append(obj)
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        for obj in dataList:
+            ops.inertVideo(obj,"webview",baseurl)
+
+        print 'seman video --解析完毕 ; channel =', channel, '; len=', len(dataList), url
+        dbVPN.commit()
+        dbVPN.close()
+
+    def parseDomVideo(self, url):
+        header = {'User-Agent':
+                  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": url}
+        try:
+            soup = self.fetchUrl(url, header)
+            div = soup.first('div',{"id":'vlink_1'})
+            if div!=None:
+                ahref = div.first('a')
+                if ahref!=None:
+                    soup = self.fetchUrl(ahref.get('href'), header)
+                    main = soup.first("div",{"class":"main playering"})
+                    if main!=None:
+                        script = main.first('script')
+                        if script!=None:
+                            match = videoApi.search(unquote(script.text))
+                            if match!=None:
+                                str= match.group(1).replace("')","").replace("$","#")
+                                alist = str.split("#")
+                                return "%s%s"%("http",alist[0])
+            print url,'没有mp4'
+            return None
+        except Exception as e:
+            print common.format_exception(e)
+            return None
+
+def videoParse(queue):
+    queue.put(VideoParse())

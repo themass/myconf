026f35ad6598043d89a7877be82283ec09828d54
liguoqing <liguoqing19861028@163.com>
2018-02-03 15:45:13 +0800

    111

--- python/fetch/xieegif_vip.py
+++ python/fetch/xieegif_vip.py
@@ -29,7 +29,7 @@ def parserneihanImg():
     imgop = imgneihanpa.ImgParse()
     imgop.run()
 if __name__ == '__main__':
-    parsewowantImg()
-    parserneihanImg()
-#     parsehugao8Img()
+#     parsewowantImg()
+#     parserneihanImg()
+    parsehugao8Img()
 #     parseratooImg()

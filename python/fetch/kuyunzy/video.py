b23757d4ce8a1fcfc7a779c582a880a5ea4d7834
liguoqing <liguoqing19861028@163.com>
2018-02-09 00:39:00 +0800

    111

--- python/fetch/kuyunzy/video.py
+++ python/fetch/kuyunzy/video.py
@@ -23,7 +23,7 @@ class VideoParse(BaseParse):
         dbVPN.commit()
         dbVPN.close()
         for item in chs:
-            for i in range(5, maxVideoPage):
+            for i in range(1, maxVideoPage):
                 url= item['url']
                 if i!=1:
                     url= "%s%s%s"%(item['url'].replace('.html','-'),i,".html")

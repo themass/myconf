890de2ec5eb5ec99c73133be9a74324c6b7da609
liguoqing <liguoqing19861028@163.com>
2018-02-10 00:55:01 +0800

    1111

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -59,12 +59,12 @@ if __name__ == '__main__':
 # # # --------------------------------------------------------------------------
 #     yezmw_vip.parseVideo()
 # #        
-    x2246_vip.parseVideo()
+#     x2246_vip.parseVideo()
 #     ax98_vip.parseVideo()
 
 #     ir6yv = ir6y.video.VideoParse()
 #     ir6yv.run()
-    urbanhenta_vip.parseVideo()
+#     urbanhenta_vip.parseVideo()
 #---------------------------------------------------------------------------- 
 #             movie
 #     tianjiyy123_vip.parseVideo()
@@ -74,7 +74,7 @@ if __name__ == '__main__':
 #     zy3838_vip.parseVideo()
 #     nomsgus_vip.parseVideo()
 #     ttkyy_vip.parseVideo()
-#     seman_vip.parseVideo()
+    seman_vip.parseVideo()
      
     #xsian
     #ttkyy

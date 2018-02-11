04c14df4e2e0b52687c9bb61b2232c47d1182b82
liguoqing <liguoqing19861028@163.com>
2018-02-11 14:51:05 +0800

    111

--- python/monitor/check_header.py
+++ python/monitor/check_header.py
@@ -1,7 +1,7 @@
 import httplib
 from urlparse import urlparse
 
-def get_status_code(url):
+def get_status_code(url,baseurl):
     """ This function retreives the status code of a website by requesting
         HEAD data from the host. This means that it only requests the headers.
         If the host cannot be reached or something else goes wrong, it returns
@@ -10,7 +10,9 @@ def get_status_code(url):
     try:
         videourl = urlparse(url)
         conn = httplib.HTTPConnection(videourl.hostname,port=videourl.port,timeout=6000)
-        conn.request("HEAD", videourl.path)
+        headers={
+                    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13', "Referer": baseurl}
+        conn.request("HEAD", videourl.path,headers=headers)
         return conn.getresponse().status
     except Exception as e :
         print e
@@ -22,7 +24,7 @@ if __name__ == '__main__':
     for line in fh.readlines():
         line.replace('\n', "").replace('\r', '')
         contents = line.split(",")
-        status = get_status_code(contents[1]) 
+        status = get_status_code(contents[1],contents[2]) 
        
         ret = "%s%s%s%s"%(contents[0],"->",status,"\n")
         print ret

b6e1a18e3505b40dd9c787c3d04eb422cefd7d78
liguoqing <liguoqing19861028@163.com>
2018-02-11 17:06:20 +0800

    111

--- python/monitor/check_header.py
+++ python/monitor/check_header.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         status = get_status_code(contents[1],contents[2]) 
        
         ret = "%s%s%s%s%s%s%s%s"%(contents[0],"->",status,"->",contents[2],"->",contents[1],"\n")
-        print ret,"------------------------",contents[2],"---",contents[1]
+        print ret
         output.write(ret)   
     output.close()
     fh.close()

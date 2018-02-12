d04522dab446b0bd8c947fd415e82e47e74a5614
liguoqing <liguoqing19861028@163.com>
2018-02-12 15:30:52 +0800

    1111

--- python/monitor/check_header.py
+++ python/monitor/check_header.py
@@ -9,7 +9,7 @@ def get_status_code(url,baseurl):
     """
     try:
         videourl = urlparse(url)
-        conn = httplib.HTTPConnection(videourl.hostname,port=videourl.port,timeout=6000)
+        conn = httplib.HTTPConnection(videourl.hostname,port=videourl.port,timeout=3000)
         headers={
                     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13', "Referer": baseurl}
         conn.request("HEAD", videourl.path,headers=headers)
@@ -20,7 +20,7 @@ def get_status_code(url,baseurl):
 
 if __name__ == '__main__':
     fh = open('../txt/video.txt')
-    output = open('/home/web/var/video_out1.txt', 'w')
+    output = open('/home/web/var/video_out2.txt', 'w')
     for line in fh.readlines():
         linestr = line.replace('\n', "").replace('\r', '')
         contents = linestr.split(",")

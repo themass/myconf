390a8327096c64b53263acd5057d86bb4025bc11
liguoqing <liguoqing19861028@163.com>
2018-03-08 02:53:56 +0800

    111

--- python/fetch/all_Image_sync.py
+++ python/fetch/all_Image_sync.py
@@ -24,16 +24,16 @@ if __name__ == '__main__':
 #     eroti_img.parseImg()
 #     singlove_vip.parseImg()
 # #     #找不到网站
-    gaoxiaogif_vip.pareImg()
-#     sexx77_vip.pareImg()
-     
-    xoxo164_vip.pareImg()
-    x2246_vip.parseImg()
-#     #需要翻墙
-    jjr128_vip.pareImg()
-    skswk9_img.parseImg()
-    
-    
+#     gaoxiaogif_vip.pareImg()
+# #     sexx77_vip.pareImg()
+#      
+#     xoxo164_vip.pareImg()
+#     x2246_vip.parseImg()
+# #     #需要翻墙
+#     jjr128_vip.pareImg()
+#     skswk9_img.parseImg()
+#     
+#     
     
     se8_vip.startWork()
     se8_vip.parseGirlImg()

e26493d32d47e5e099fafff25e911f2888d7c117
liguoqing <liguoqing19861028@163.com>
2018-03-12 23:05:20 +0800

    111

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -52,37 +52,37 @@ if __name__ == '__main__':
 # # ------------------------------------------------------------------
 #     se8_vip.parseVideo()
       
-    sexx77_vip.parseVideo()
-# # # #     
-# #  
-    xoxo164videop = xoxo164.video.VideoParse()
-    xoxo164videop.run()
-# # # 
-    singlovevideop = singlove.video.VideoParse()
-    singlovevideop.run()
-# # # --------------------------------------------------------------------------
-    yezmw_vip.parseVideo()
-#        
-    x2246_vip.parseVideo()
-    ax98_vip.parseVideo()
-  
-    ir6yv = ir6y.video.VideoParse()
-    ir6yv.run()
-#     urbanhenta_vip.parseVideo()
-
-    iir44_vip.parseVideo()
-    tzzy1399_vip.parseVideo()
-    wuji3_vip.parseVideo()
-# #---------------------------------------------------------------------------- 
-# #             movie
-    tianjiyy123_vip.parseVideo()
+#     sexx77_vip.parseVideo()
+# # # # #     
+# # #  
+#     xoxo164videop = xoxo164.video.VideoParse()
+#     xoxo164videop.run()
+# # # # 
+#     singlovevideop = singlove.video.VideoParse()
+#     singlovevideop.run()
+# # # # --------------------------------------------------------------------------
+#     yezmw_vip.parseVideo()
+# #        
+#     x2246_vip.parseVideo()
+#     ax98_vip.parseVideo()
+#   
+#     ir6yv = ir6y.video.VideoParse()
+#     ir6yv.run()
+# #     urbanhenta_vip.parseVideo()
+# 
+#     iir44_vip.parseVideo()
+#     tzzy1399_vip.parseVideo()
+#     wuji3_vip.parseVideo()
+# # #---------------------------------------------------------------------------- 
+# # #             movie
+#     tianjiyy123_vip.parseVideo()
 # #     
 # #     
 #      
-    zy3838_vip.parseVideo()
-    nomsgus_vip.parseVideo()
-    ttkyy_vip.parseVideo()
-    seman_vip.parseVideo()
+#     zy3838_vip.parseVideo()
+#     nomsgus_vip.parseVideo()
+#     ttkyy_vip.parseVideo()
+#     seman_vip.parseVideo()
     kuyunzy_vip.parseVideo()
     kump4_vip.parseVideo()
      

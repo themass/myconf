89dc5cfacbc26a34546cdb015043e18f2846b259
themass <liguoqing19861028@163.com>
2018-03-15 00:00:19 +0800

    Update db_ops.py

--- python/common/db_ops.py
+++ python/common/db_ops.py
@@ -135,14 +135,14 @@ class DbOps(object):
         return self.conn.execute(
             "replace into  videochannel (name,url,baseurl,updateTime,rate,showtype,enable,channel,channelType) values ('%s','%s','%s','%s',%s,%s,%s,'%s','%s')"
             % (
-                obj.get("name"), obj.get("url"), obj.get("baseurl"), obj.get("updateTime"), 1.2, obj.get("showType"), 1, obj.get("channel"), obj.get("channelType")))
+                obj.get("name"), obj.get("url"), obj.get("baseurl"), obj.get("updateTime"), 1.2, obj.get("showType"), 1, obj.get("channel").replace(".com",'-'), obj.get("channelType")))
 
     def inertVideo(self, obj,videoType="normal",baseUrl=''):
         sortType = dateutil.y_m_d()
         return self.conn.execute(
             "replace into  videoitems (name,url,channel,pic,updateTime,path,videoType,baseUrl,sortType) values ('%s','%s','%s','%s','%s','%s','%s','%s','%s')"
             % (
-                obj.get("name"), obj.get("url"), obj.get("channel"), obj.get("pic"), obj.get("updateTime"), obj.get("path"), videoType,baseUrl,sortType))
+                obj.get("name"), obj.get("url"), obj.get("channel").replace(".com",'-'), obj.get("pic"), obj.get("updateTime"), obj.get("path"), videoType,baseUrl,sortType))
 
     def inertVideoWebView(self, obj):
         return self.conn.execute(

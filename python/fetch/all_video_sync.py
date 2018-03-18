8ac860b848ec83e48d95c1ef0b82cb8b06cb49ee
liguoqing <liguoqing19861028@163.com>
2018-03-18 09:38:56 +0800

    111

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -50,25 +50,25 @@ if __name__ == '__main__':
 #     fff64_vip.parseVideo()
 # #  
 # # # ------------------------------------------------------------------
-#     se8_vip.parseVideo()
+#     ##se8_vip.parseVideo()
        
-    sexx77_vip.parseVideo()
-# # # # #     
-# # #  
-    xoxo164videop = xoxo164.video.VideoParse()
-    xoxo164videop.run()
-# # # 
-    singlovevideop = singlove.video.VideoParse()
-    singlovevideop.run()
-# # # --------------------------------------------------------------------------
-    yezmw_vip.parseVideo()
-#        
-    x2246_vip.parseVideo()
-    ax98_vip.parseVideo()
-   
-    ir6yv = ir6y.video.VideoParse()
-    ir6yv.run()
-#     urbanhenta_vip.parseVideo()
+#     sexx77_vip.parseVideo()
+# # # # # #     
+# # # #  
+#     xoxo164videop = xoxo164.video.VideoParse()
+#     xoxo164videop.run()
+# # # # 
+#     singlovevideop = singlove.video.VideoParse()
+#     singlovevideop.run()
+# # # # --------------------------------------------------------------------------
+#     yezmw_vip.parseVideo()
+# #        
+#     x2246_vip.parseVideo()
+#     ax98_vip.parseVideo()
+#    
+#     ####ir6yv = ir6y.video.VideoParse()
+#     ir6yv.run()
+#     ##urbanhenta_vip.parseVideo()
  
     iir44_vip.parseVideo()
     tzzy1399_vip.parseVideo()

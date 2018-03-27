d9926f2c86a47ac3b444b44a5034c1c488cfac8b
liguoqing <liguoqing19861028@163.com>
2018-03-27 22:21:50 +0800

    11

--- python/fetch/test.py
+++ python/fetch/test.py
@@ -24,7 +24,18 @@ str1 = '''
 <iframe width="640" height="360" src="https://weav.cc/embed/892fbfd1c5a5b457e577" frameborder="0" allowfullscreen></iframe>
 
 '''
+
+def fetchUrl(url):
+    req = urllib2.Request(url, headers={'Cookie':"td_cookie=18446744069599001696; UM_distinctid=16267a77486203-0a34f7eb9f837-454c092b-1fa400-16267a7748726f; CNZZDATA4033785=cnzz_eid%3D1967344694-1522153663-null%26ntime%3D1522153663; CNZZDATA1263493226=2025093065-1522155903-null%7C1522155903; PHPSESSID=cqppj1tg9v8tf27j95ogqogjs1; td_cookie=18446744069599206493; WSKY=6c172; jiathis_rdc=%7B%22http%3A//www.zxdy.cc/vod/22266.html%22%3A1739039602%2C%22http%3A//www.zxdy.cc/play/22266-0-1.html%22%3A1739044927%2C%22http%3A//www.zxdy.cc/Uploads/https%3A//tupian.tupianzy.com/pic/upload/vod/2018-03-03/201803031520062617.jpg%22%3A1739118415%2C%22http%3A//www.zxdy.cc/list/1-p-3-0.html%22%3A1739129605%2C%22http%3A//www.zxdy.cc/list/1-p-1-0.html%22%3A1739216767%2C%22http%3A//www.zxdy.cc/list/9-p-1-0.html%22%3A1739358031%2C%22http%3A//www.zxdy.cc/list/9-p-2-0.html%22%3A1739371664%2C%22http%3A//www.zxdy.cc/Uploads/https%3A//wx3.sinaimg.cn/mw690/005w5c6ogy1fjuo496v5uj30tu15ok3k.jpg%22%3A1739577535%2C%22http%3A//www.zxdy.cc/Uploads/https%3A//img.alicdn.com/imgextra/i4/2264228004/TB2UynHnQqvpuFjSZFhXXaOgXXa_%21%212264228004.jpg%22%3A1739585958%2C%22http%3A//www.zxdy.cc/%22%3A1739586271%2C%22http%3A//www.zxdy.cc/vod/5128.html%22%3A1739763188%2C%22http%3A//www.zxdy.cc/vod/1.html%22%3A1739772004%2C%22http%3A//www.zxdy.cc/play/1-0-1.html%22%3A1739777508%2C%22http%3A//www.zxdy.cc/vod/4063.html%22%3A1739811363%2C%22http%3A//www.zxdy.cc/play/4063-0-2.html%22%3A1739820736%2C%22http%3A//www.zxdy.cc/list/11-p-1-0.html%22%3A1739855919%2C%22http%3A//www.zxdy.cc/vod/22236.html%22%3A0%7C1522158279843%2C%22http%3A//www.zxdy.cc/play/22236-0-1.html%22%3A%220%7C1522158307282%22%7D",
+                'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13', "Referer":url})
+    req.encoding = 'utf-8'
+    response = urllib2.urlopen(req, timeout=300)
+    gzipped = response.headers.get(
+        'Content-Encoding')  # 查看是否服务器是否支持gzip
+    content = response.read().decode('utf8', errors='replace')
+    print content
 if __name__ == '__main__':
+    fetchUrl("http://www.zxdy.cc/list/11-p-1-0.html")
     #     name = '第95期<!--[if lt IE 9 ]><![endif]-->2017/9/9<!--[if lt IE 9 ]><![endif]-->'
     #     strName = name.replace(
     #         "<!--[if lt IE 9 ]>", "").replace("<![endif]-->", "")
@@ -75,11 +86,11 @@ if __name__ == '__main__':
 #     regVideo = re.compile(r"mac_url=unescape\('(.*)(http)(.*)'\);")
 #     match = regVideo.search(str)
 #     str2 = '(.*)'
-    str = '''
-var mac_flag='play',mac_link='/g/41818/{src}/{num}.html', mac_name='速度与激情8',mac_from='m3u8',mac_server='no',mac_note='',mac_url=unescape('HD1280%u9ad8%u6e05%u4e2d%u5b57%u97e9%u7248$http://fuli.yazyzw.com/20170603/XgwO7tMy/index.m3u8#BD1280%u9ad8%u6e05%u4e2d%u82f1%u53cc%u5b57%u7248$http://hao.zuida-youku.com/20170609/NoFPAsy5/index.m3u8');
-''' 
-    videoApi = re.compile(r"http(.*)\.m3u8")
-    match = videoApi.search(str)
-    print match
-    print match.group(1).replace("')","")
-    print unquote(str)
+#     str = '''
+# var mac_flag='play',mac_link='/g/41818/{src}/{num}.html', mac_name='速度与激情8',mac_from='m3u8',mac_server='no',mac_note='',mac_url=unescape('HD1280%u9ad8%u6e05%u4e2d%u5b57%u97e9%u7248$http://fuli.yazyzw.com/20170603/XgwO7tMy/index.m3u8#BD1280%u9ad8%u6e05%u4e2d%u82f1%u53cc%u5b57%u7248$http://hao.zuida-youku.com/20170609/NoFPAsy5/index.m3u8');
+# ''' 
+#     videoApi = re.compile(r"http(.*)\.m3u8")
+#     match = videoApi.search(str)
+#     print match
+#     print match.group(1).replace("')","")
+#     print unquote(str)
---
+++ python/fetch/youjiady/video.py
@@ -0,0 +1,105 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+from baseparse import *
+from urlparse import urlparse
+from common import common
+from urllib import unquote
+import time
+from fetch.profile import *
+class VideoParse(BaseParse):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        chs = self.videoChannel()
+        for item in chs:
+            ops.inertVideoChannel(item)
+        print 'youjiady video -- channel ok;,len=',len(chs)
+        dbVPN.commit()
+        dbVPN.close()
+        for item in chs:
+            for i in range(1, maxVideoPage):
+                url= "/%s%s%s%s"%(item['url'],"&","page=",i)
+                print url
+                self.videoParse(item['channel'], url)
+                print '解析完成 ', item['channel'], ' ---', i, '页'
+    def videoChannel(self):
+        ahrefs = self.header()
+        channelList = []
+        for ahref in ahrefs:
+            obj={}
+            obj['name']=ahref.text
+            obj['url']=ahref.get('href')
+            obj['baseurl']=baseurl
+            obj['updateTime']=datetime.datetime.now()
+            obj['pic']=''
+            obj['rate']=1.2
+            obj['channel']=obj['name']
+            obj['showType']=3
+            obj['channelType']='normal'
+            channelList.append(obj)
+        return channelList
+    def videoParse(self, channel, url):
+        dataList = []
+        soup = self.fetchUrl(url)
+        alist = soup.findAll("a", {"class": "videopic lazy"})
+        for ahref in alist:
+            obj = {}
+            mp4Url = self.parseDomVideo(ahref.get("href"))
+            if mp4Url == None:
+                print '没有mp4 文件:', ahref.get("href")
+                continue
+            obj['url'] = mp4Url
+            obj['pic'] = ahref.get("data-original")
+            obj['name'] = ahref.get('title')
+            print obj['name'],mp4Url,obj['pic']
+
+            videourl = urlparse(obj['url'])
+            obj['path'] = videourl.path
+            obj['updateTime'] = datetime.datetime.now()
+            obj['channel'] = channel
+            dataList.append(obj)
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        for obj in dataList:
+            ops.inertVideo(obj,"normal",baseurl)
+
+        print '44iir video --解析完毕 ; channel =', channel, '; len=', len(dataList), url
+        dbVPN.commit()
+        dbVPN.close()
+
+    def parseDomVideo(self, url):
+        header = {'User-Agent':
+                  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": url}
+        try:
+            soup = self.fetchUrl(url, header)
+            print soup
+            div = soup.first("div",{'class':'panel clearfix'})
+            if div!=None:
+                adiv = div.first("div")
+                if adiv!=None:
+                    ahref = adiv.first('a')
+                    if ahref!=None:
+                        soup = self.fetchUrl(ahref.get('href'), header)
+                        play_video = soup.first('div',{'class':'info clearfix'})
+                        if play_video!=None:
+                            script = play_video.first('script')
+                            if script!=None:
+                                text = script.text
+                                texts = text.split("$")
+                                for item in texts:
+                                    match = regVideo.search(item)
+                                    if match!=None:
+                                        videoUrl =match.group(1)
+                                        return "%s%s%s"%("http",videoUrl,'m3u8')
+            print '没找到mp4'
+            return None
+        except Exception as e:
+            print common.format_exception(e)
+            return None
+
+def videoParse(queue):
+    queue.put(VideoParse())

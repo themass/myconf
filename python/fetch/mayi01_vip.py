d5c0e3bad3aefab46a2f48dae1423886ec867749
liguoqing <liguoqing19861028@163.com>
2018-07-14 02:32:10 +0800

    111

---
+++ python/fetch/mayi01_vip.py
@@ -0,0 +1,27 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+import datetime
+import urllib2
+import threading
+from BeautifulSoup import BeautifulSoup
+from common.envmod import *
+from common import common
+from common import typeutil
+from common import db_ops
+from common import MyQueue
+from mayi01 import *
+import re
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def parseVideo():
+    videop = video.VideoUserParse()
+    videop.run()
+def parseText():
+    textpo = text.TextChannelParse()
+    textpo.run()
+if __name__ == '__main__':
+    parseText()
+    parseVideo()
--- python/fetch/profile.py
+++ python/fetch/profile.py
@@ -1,5 +1,5 @@
 #!/usr/bin python
 # -*- coding: utf-8 -*-
-maxVideoPage=20
+maxVideoPage=4
 maxImgPage = 3
 maxTextPage=15

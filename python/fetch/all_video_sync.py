6bdf76b354b8ee70cccaa05a60009ec8207fe736
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-07-25 00:41:57 +0800

    11

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -112,7 +112,7 @@ def pase5():
     hanpian_vip.parseVideo()
     
 def pase6():
-    se8_vip.parseVideo()
+    #se8_vip.parseVideo()
     ppyy55_vip.parseUserVideo()
     s58589_vip.parseVideo()
     qh_vip.parseVideo()

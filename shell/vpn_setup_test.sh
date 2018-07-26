f62ba4aec6514e827663b7624847dde821084fcf
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-07-27 01:27:09 +0800

    111

--- shell/vpn_setup_test.sh
+++ shell/vpn_setup_test.sh
@@ -3,32 +3,84 @@
 ## Version setting
 ## -----------------------
 
-#init soft
 
-#http://it.zhaozhao.info/archives/41127
-#https://segmentfault.com/a/1190000002540601
-#http://www.cnblogs.com/hyzhou/category/336618.html
 WORKDIR=/root/work
-TMP_HOME=/root/soft/temp
+TMP_HOME=/root/soft
 PWD=`pwd`
+#ip=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|grep -v 10. |awk '{print $1}'|tr -d "addr:"`
 init_soft() 
 {
-	echo 'init_soft'
+	mkdir -p ${WORKDIR}
+	mkdir -p ${TMP_HOME}
+	apt-get update
+	apt-get install -y sysstat vim build-essential lrzsz  tree dstat git dos2unix unzip libtalloc2   libtalloc-dev libxml2-dev php-pear aptitude
+	aptitude install libgmp10 libgmp3-dev libssl-dev pkg-config libpcsclite-dev libpam0g-dev  curl   libmysqlclient-dev 
+	apt-get install libcurl4-gnutls-dev
 }
-
+## -----------------------
+## Setup all aboves
+## -----------------------
+check_vpn() 
+{
+    	iptables -L 
+	ipsec status | grep Associations
+	ps -aux| grep fail | grep -v 'grep'
+	ps -aux| grep telegraf| grep -v 'grep'
+}
+checkspeed()
+{
+	cd ${TMP_HOME}
+	wget https://raw.githubusercontent.com/wn789/Superspeed/master/superbench.sh
+	bash superbench.sh
+	wget  https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py 
+ 	python speedtest.py 
+}
 strongswan_setup() 
 {
-	echo 'strongswan_setup'
+
+	#sudo apt-get install strongswan strongswan-pki libcharon-extra-plugins libstrongswan-extra-plugins
+	cd ${TMP_HOME}
+	wget http://download.strongswan.org/strongswan-5.6.0.tar.bz2 --no-check-certificate
+	tar -jxvf strongswan-5.6.0.tar.bz2 && cd strongswan-5.6.0
+	./configure --prefix=/usr --sysconfdir=/etc  --enable-openssl --enable-nat-transport --disable-mysql --disable-ldap  --disable-static --enable-shared --enable-md4 --enable-eap-mschapv2 --enable-eap-aka --enable-eap-aka-3gpp2  --enable-eap-gtc --enable-eap-identity --enable-eap-md5 --enable-eap-peap --enable-eap-radius --enable-eap-sim --enable-eap-sim-file --enable-eap-simaka-pseudonym --enable-eap-simaka-reauth --enable-eap-simaka-sql --enable-eap-tls --enable-eap-tnc --enable-eap-ttls
+	make && make install
+	ipsec start 
+	cd ..
 }
 strongswan_config() 
 {
-	echo 'strongswan_config'
+	#unzip strongswan_conf.zip
+	#dos2unix strongswan_conf/ipsec.conf
+	#dos2unix strongswan_conf/charon.conf
+	#dos2unix strongswan_conf/ipsec.secrets
+	#
+	#cp  /etc/ipsec.conf /etc/ipsec.conf.bak
+	#cp  strongswan_conf/ipsec.conf /etc/ipsec.conf
+    #
+	#cp  /etc/strongswan.d/charon.conf /etc/strongswan.d/charon.conf.bak
+	#cp  strongswan_conf/charon.conf /etc/strongswan.d/charon.conf
+    #
+	#cp  /etc/ipsec.secrets  /etc/ipsec.secrets.bak
+	#cp  strongswan_conf/ipsec.secrets /etc/ipsec.secrets
+	
+	cd ${WORKDIR}/myconf/shell
+	cp  /etc/ipsec.conf /etc/ipsec.conf.bak
+	cp  ../strongswan_conf/ipsec.conf /etc/ipsec.conf
+    
+	cp  /etc/strongswan.d/charon.conf /etc/strongswan.d/charon.conf.bak
+	cp  ../strongswan_conf/charon.conf /etc/strongswan.d/charon.conf
+    
+	cp  /etc/ipsec.secrets  /etc/ipsec.secrets.bak
+	cp  ../strongswan_conf/ipsec.secrets /etc/ipsec.secrets
+	
+	cp  ../strongswan_conf/strongswan.conf /etc/strongswan.conf
+	
+	ipsec restart
 }
 #ca setup
 init_ca() 
 {
 	cd ${TMP_HOME}
-	echo $1
 	mkdir -p ca
 	cd ca
 	echo "C=CN, O=timeline, CN=$1"
@@ -43,15 +95,79 @@ init_ca()
 
 	openssl pkcs12 -export -inkey clientKey.pem -in clientCert.pem -name "client" -certfile caCert.pem -caname "$1" -out clientCert.p12
 
+	cp caCert.pem /etc/ipsec.d/cacerts/
+	cp serverCert.pem /etc/ipsec.d/certs/
+	cp serverKey.pem /etc/ipsec.d/private/
+	cp clientCert.pem /etc/ipsec.d/certs/
+	cp clientKey.pem /etc/ipsec.d/private/
+	ipsec restart
 }
 
 
 # iptables
 setup_iptables() 
 {
-	echo $1
+	iptables -A INPUT -p udp --dport 500 -j ACCEPT 
+	iptables -A INPUT -p udp --dport 4500 -j ACCEPT 
+	iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o $1 -j MASQUERADE 
+	iptables -A FORWARD -s 10.0.0.0/24 -j ACCEPT 
+	iptables -A FORWARD -d 10.0.0.0/24 -j ACCEPT
+	ip6tables -A INPUT -p udp --dport 4500 -m frag --fragfirst -j CONNMARK --set-mark 0x42
+	ip6tables -A INPUT -p udp --dport 4500 -j ACCEPT
+	ip6tables -A INPUT -m frag -m connmark --mark 0x42 -j ACCEPT
+	#service iptables save
+	#service iptables restart
+	#systemctl restart iptables
+	iptables-save
+	#iptables-restore  https://blog.csdn.net/hack8/article/details/6772958
+	#tcpdump -s 0 -n -i eth0 'esp or udp and (port 500 or port 4500)'
 }
+#net
+# vi /etc/sysctl.conf
+net() 
+{
 
+	echo "net.ipv4.tcp_syncookies = 1"  >>  /etc/sysctl.conf
+	echo "net.ipv4.tcp_tw_reuse = 1"  >>  /etc/sysctl.conf
+	echo "net.ipv4.tcp_tw_recycle = 0"  >>  /etc/sysctl.conf
+	echo "net.ipv4.ip_local_port_range = 1024 65000 "  >>  /etc/sysctl.conf
+	echo "net.ipv4.tcp_max_syn_backlog = 8192 "  >>  /etc/sysctl.conf
+	echo "net.ipv4.tcp_max_tw_buckets = 5000"  >>  /etc/sysctl.conf
+	echo "net.ipv4.tcp_keepalive_time = 1200"  >>  /etc/sysctl.conf
+	echo "net.ipv4.tcp_fin_timeout = 30"  >>  /etc/sysctl.conf
+	echo "net.ipv4.tcp_max_tw_buckets = 5000"  >>  /etc/sysctl.conf
+	echo "net.ipv4.tcp_rmem = 4096 87380 4194304"  >>  /etc/sysctl.conf
+	echo "net.ipv4.tcp_wmem = 4096 16384 4194304"  >>  /etc/sysctl.conf
+	echo "net.core.rmem_max = 16777216"  >>  /etc/sysctl.conf
+	echo "net.core.wmem_max = 16777216"  >>  /etc/sysctl.conf
+	echo "net.core.netdev_max_backlog = 262144"  >>  /etc/sysctl.conf
+	echo "net.core.somaxconn = 262144"  >>  /etc/sysctl.conf
+	echo "net.ipv4.ip_forward = 1"  >>  /etc/sysctl.conf
+	echo "net.ipv6.conf.all.forwarding=1"  >>  /etc/sysctl.conf
+	
+	# max open files
+	echo "fs.file-max = 1024000"  >>  /etc/sysctl.conf
+	
+	cat /etc/sysctl.conf
+	sysctl -p
+	
+	#sysctl net.ipv4.tcp_available_congestion_control
+	echo "*               soft    nofile           512000"  >> /etc/security/limits.conf
+	echo "*               hard    nofile          1024000"  >> /etc/security/limits.conf
+	echo "ulimit -SHn 1024000"  >> /root/.profile
+	source /root/.profile
+	ulimit -n
+
+}
+## -----------------------
+## Setup all aboves
+## -----------------------
+setup_fail2ban() 
+{
+    apt-get install -y fail2ban sendmail
+    cp ../monitor/jail.conf /etc/fail2ban/
+    service fail2ban restart
+}
 ## -----------------------
 ## Setup all aboves
 ## -----------------------
@@ -64,19 +180,19 @@ setup_all()
     init_ca	$ip
     dev=$(get_netdev)
     setup_iptables $dev
+    setup_fail2ban
+    net
 }
-get_ip()
-{
+get_ip(){
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
-    [ ! -z ${IP} ] && echo ${IP}
+    [ ! -z ${IP} ] && echo ${IP} 
     echo ${IP}
 }
-get_netdev()
-{
+get_netdev(){
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
-    DEV=$( ip addr | grep ${IP} |egrep -o '(eth0|eth1|ens3)')
+    DEV=$( ip addr | grep ${IP} ||egrep -o '(eth0|ens3|enp1s0)')
     echo ${DEV}
 }
 ## -----------------------

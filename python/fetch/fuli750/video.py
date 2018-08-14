5d5e1f9474140224512a5b458b21de687c9027d6
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-08-15 00:15:25 +0800

    11

--- python/fetch/fuli750/video.py
+++ python/fetch/fuli750/video.py
@@ -22,7 +22,7 @@ class VideoParse(BaseParse):
         dbVPN.commit()
         dbVPN.close()
         for ch in chs:
-            for i in range(1, maxVideoPage):
+            for i in range(42, maxVideoPage):
                 url= ch['url']
                 if i!=1:
                     url= "%s%s%s"%(ch['url'],"?page=",i)

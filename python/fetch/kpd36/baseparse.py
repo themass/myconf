a5d1fe57fae7faebce3aad5718c17a6489883854
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-08-18 09:48:38 +0800

    111

--- python/fetch/kpd36/baseparse.py
+++ python/fetch/kpd36/baseparse.py
@@ -11,7 +11,7 @@ import threading
 from BeautifulSoup import BeautifulSoup
 import re
 import os
-baseurl = "http://www.36kpd.com"
+baseurl = "http://www.kpd82.com"
 
 header = {'Cookie':'UM_distinctid=16529ac59740-084dfe7512bd8e-47e1039-1fa400-16529ac5977243; fikker-4gJQ-VSIJ=MQls4GDSRcImxuLcNJd7NMxLDStI0srq; fikker-4gJQ-VSIJ=MQls4GDSRcImxuLcNJd7NMxLDStI0srq; CNZZDATA1274319095=208496749-1534001853-null%7C1534006406; CNZZDATA1274141679=1940873741-1533998159-null%7C1534007965; fikker-NujU-rLFW=Oktg0GvorcheDc25xWRmRLjSVwb76kBS; fikker-NujU-rLFW=Oktg0GvorcheDc25xWRmRLjSVwb76kBS; fikker-d1fm-MUvU=NQ6e2qFujuVQTo2qtERKx3ICpoH9fchq; fikker-d1fm-MUvU=NQ6e2qFujuVQTo2qtERKx3ICpoH9fchq; fikker-8INA-OGRo=riJqBoXYrU6EpUdOOgpqNktLfEbLLu89; fikker-8INA-OGRo=riJqBoXYrU6EpUdOOgpqNktLfEbLLu89; fikker-Mj9K-O6Kl=cTkDYNQ47bUxNRUnIxaTurmNe8ETiFyT; fikker-Mj9K-O6Kl=cTkDYNQ47bUxNRUnIxaTurmNe8ETiFyT; vcnzdmlusername=themass; vcnzdmluserid=194056; vcnzdmlgroupid=1; vcnzdmlrnd=HcBPFRl3teW9rN8MMoKw; vcnzdmlauth=d8e8ccc200438e3b766af8f7ea6b4bb5; vcnzdcheckplkey=1534011045%2Ce794e15a9307a7c6e49289b21165fa85%2Cd3b151f56d8ec4483d5346e665d2389e','User-Agent':
           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": baseurl}

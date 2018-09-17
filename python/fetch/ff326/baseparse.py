6190507b1d75b8f4c49b18a03fed428c24a972bd
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-09-18 00:32:32 +0800

    111

--- python/fetch/ff326/baseparse.py
+++ python/fetch/ff326/baseparse.py
@@ -11,7 +11,7 @@ import threading
 from BeautifulSoup import BeautifulSoup
 import re
 import os
-baseurl = "http://326ff.com"
+baseurl = "http://104yu.com"
 header = {'User-Agent':
           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": baseurl}
 maxCount = 3

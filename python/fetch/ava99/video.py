963fea1f43881c44b00d9079036594367867626f
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-11-25 14:12:55 +0800

    111

---
+++ python/fetch/ava99/video.py
@@ -0,0 +1,106 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+from baseparse import *
+from urlparse import urlparse
+from common import common
+from urllib import unquote
+import time
+from fetch.profile import *
+class VideoParse(BaseParse):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        chs = self.videoChannel()
+        for item in chs:
+            ops.inertVideoChannel(item)
+        print 'bt2n video -- channel ok;,len=',len(chs)
+        dbVPN.commit()
+        dbVPN.close()
+        for item in chs:
+            for i in range(1, maxVideoPage):
+                url = item['url']
+                if i!=1:
+                    url= "%s%s%s%s"%(item['url'],"index-",i,".html")
+                print url
+                self.videoParse(item['channel'], url,item['baseurl'])
+                print '解析完成 ', item['baseurl'],item['channel'], ' ---', i, '页'
+                time.sleep(1)
+    def videoChannel(self):
+        channelList = []
+        ahrefs = self.header("header.html")
+        for ahref in ahrefs:
+            obj={}
+            obj['name']=ahref.text
+            obj['url']=ahref.get('href')
+            obj['baseurl']=baseurl
+            obj['updateTime']=datetime.datetime.now()
+            obj['pic']=''
+            obj['rate']=1
+            obj['channel']='ava99'+ahref.text
+            obj['showType']=3
+            obj['channelType']='ava99_all'
+            channelList.append(obj)
+        channelList.reverse()
+        return channelList
+    def videoParse(self, channel, url,base):
+        dataList = []
+        soup = self.fetchUrl(url)
+        metas = soup.findAll("li",{"class":"mb-item"})
+        for meta in metas:
+            obj = {}
+            ahref = meta.first("a")
+            mp4Url = self.parseDomVideo(base,ahref.get("href"))
+            if mp4Url == None:
+                print '没有mp4 文件:', ahref.get("href")
+                continue
+            obj['url'] = mp4Url
+            obj['pic'] = meta.first('div',{"class":"mb-img"}).get("style").replace("background-image:url(","").replace(");","")
+            obj['name'] = meta.first('span').text
+
+            videourl = urlparse(mp4Url)
+            obj['path'] = 'ava99_'+videourl.path
+            obj['updateTime'] = datetime.datetime.now()
+            obj['channel'] = channel
+            obj['videoType'] = "normal"
+            obj['baseurl'] = baseurl
+            print obj['name'],obj['videoType'],obj['url'],obj['pic']
+            dataList.append(obj)
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        for obj in dataList:
+            ops.inertVideo(obj,obj['videoType'],baseurl)
+
+        print 'qh video --解析完毕 ; channel =', channel, '; len=', len(dataList), url
+        dbVPN.commit()
+        dbVPN.close()
+
+    def parseDomVideo(self, base,url):
+        try:
+            soup = self.fetchUrlWithBase(base+url, header)
+            div = soup.first("p",{'class':'act g-clear'})
+            if div!=None:
+                ahref = div.first('a')
+                if ahref!=None:
+                    soup = self.fetchUrl(ahref.get("href"),header)
+                    scripts = soup.findAll('script')
+                    for script in scripts:
+                        if script.get("src").count("upload/playdata")>0:
+                            text = unquote(self.fetchContentUrl(script.get("src"),header))
+                            texts = text.split("$")
+                            for item in texts:
+                                match = regVideo.search(item)
+                                if match!=None:
+                                    videoUrl =match.group(1)
+                                    return "%s%s%s"%("http",videoUrl,'m3u8')
+            print '没找到mp4'
+            return None
+        except Exception as e:
+            print common.format_exception(e)
+            return None
+
+def videoParse(queue):
+    queue.put(VideoParse())
--- python/fetch/profile.py
+++ python/fetch/profile.py
@@ -1,5 +1,5 @@
 #!/usr/bin python
 # -*- coding: utf-8 -*-
-maxVideoPage=3
+maxVideoPage=40
 maxImgPage = 5
 maxTextPage=3

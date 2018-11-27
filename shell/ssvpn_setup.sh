d6a00953e180015cd57711ab6b937e18c21648a4
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-11-27 22:34:10 +0800

    111

--- shell/ssvpn_setup.sh
+++ shell/ssvpn_setup.sh
@@ -32,7 +32,7 @@ setup_ssr()
 	cd ${WORKDIR}
 	#	git clone -b manyuser https://github.com/ToyoDAdoubi/shadowsocksr.git
 	
-	git clone -b https://github.com/themass/shadowsocksr.git
+	git clone  https://github.com/themass/shadowsocksr.git
 	cd shadowsocksr
 	bash setup_cymysql.sh
 	bash initcfg.sh

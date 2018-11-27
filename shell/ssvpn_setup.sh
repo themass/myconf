9b7c4b8b1bf3bc2d5c6667461cc0a5fac18d4fb5
themass <liguoqing19861028@163.com>
2018-11-27 22:27:30 +0800

    Update ssvpn_setup.sh

--- shell/ssvpn_setup.sh
+++ shell/ssvpn_setup.sh
@@ -30,7 +30,7 @@ setup_sspanel()
 setup_ssr() 
 {
 	cd ${WORKDIR}
-	git clone -b manyuser https://github.com/ToyoDAdoubi/shadowsocksr.git
+	git clone -b manyuser https://github.com/themass/shadowsocksr.git
 	cd shadowsocksr
 	bash setup_cymysql.sh
 	bash initcfg.sh

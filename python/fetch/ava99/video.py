82e989640eb32ac83f0395eb5e2a8c68687fd693
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-12-14 22:05:32 +0800

    11

--- python/fetch/ava99/video.py
+++ python/fetch/ava99/video.py
@@ -100,7 +100,7 @@ class VideoParse(BaseParse):
                                 match = regVideo.search(item)
                                 if match!=None:
                                     videoUrl =match.group(1)
-                                    return "%s%s%s"%("http",videoUrl,'m3u8')
+                                    return "%s%s%s"%("http",videoUrl,'.m3u8')
             print '没找到mp4'
             return None
         except Exception as e:

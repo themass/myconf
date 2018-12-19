68b8b89a87cbb8d4f00293d865a486aecea91886
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-12-20 00:21:17 +0800

    111

--- python/fetch/sexx77/baseparse.py
+++ python/fetch/sexx77/baseparse.py
@@ -11,7 +11,7 @@ import threading
 from BeautifulSoup import BeautifulSoup
 import re
 # http://www.dehyc.com
-baseurl = "https://www.88guise.com"
+baseurl = "https://www.guise66.com"
 header = {'User-Agent':
           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": baseurl}
 maxCount = 3

a3ac1b74a10445749b4983392cb05ac91e3123b0
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-12-20 00:23:14 +0800

    11

--- python/fetch/sexx77_vip.py
+++ python/fetch/sexx77_vip.py
@@ -14,39 +14,13 @@ import re
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
-queue = MyQueue.MyQueue(200000)
-maxCount = 5
 
-class HandleThread(threading.Thread):
-
-    def __init__(self, name, queue):
-        threading.Thread.__init__(self, name=name)
-        self.t_name = name
-        self.t_queue = queue
-
-    def run(self):
-        while(True):
-            try:
-                print queue.qsize()
-                obj = queue.get(timeout=30)
-                if obj != None:
-                    obj.run()
-            except Exception as e:
-                print common.format_exception(e)
-                pass
-def pareImg():
-    imgrun = img.ImgParse()
-    imgrun.run()
-#     queue.put(img.ImgParse(obj))
 def parseVideo():
     videop = video.VideoParse()
     videop.run()
-    
-def parseText():
-    textop = text.TextChannelParse()
-    textop.run()
+
 if __name__ == '__main__':
-    parseText()
+    parseVideo()
     
 #     textop = text.TextChannelParse()
 #     textop.run()

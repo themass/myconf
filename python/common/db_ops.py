195c88fda02c4724f88c3d58ac578fb164c2006c
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-12-23 00:28:20 +0800

    111

--- python/common/db_ops.py
+++ python/common/db_ops.py
@@ -82,7 +82,7 @@ class DbOps(object):
 
     def inertImgItems_item(self, obj):
         return self.conn.execute(
-            "replace ignore  into  imgitems_item (picUrl,origUrl,itemUrl) values ('%s','%s','%s')"
+            "insert  ignore  into  imgitems_item (picUrl,origUrl,itemUrl) values ('%s','%s','%s')"
             % (obj.get("picUrl"), obj.get("origUrl"), obj.get("itemUrl")))
 
     def getImgItems_itemUnSyncById(self, ids):

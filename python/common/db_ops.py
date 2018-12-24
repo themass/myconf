24c527c208a85d476af2c602f28b2572ef66df3d
liguoqing19861028@163.com <liguoqing19861028@163.com>
2018-12-24 23:10:16 +0800

    111

--- python/common/db_ops.py
+++ python/common/db_ops.py
@@ -52,6 +52,7 @@ class DbOps(object):
     def getTextChannelItemsById(self, i, sortType):
         start = i * 40
         end = (i + 1) * 40
+        print "select i.file file,t.url url ,t.id id from  textitems_item i, textitems t where i.fileUrl=t.url and t.sortType='%s' order by i.id desc  limit %s,%s " % (sortType, start, end)
         self.conn.execute(
             "select i.file file,t.url url ,t.id id from  textitems_item i, textitems t where i.fileUrl=t.url and t.sortType='%s' order by i.id desc  limit %s,%s " % (sortType, start, end))
         return self.conn.fetchAll()

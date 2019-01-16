655a468b47801b9f0b86a43ebf8ab58c343bf6a5
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-01-16 12:32:45 +0800

    11

--- python/fetch/skswk9/baseparse.py
+++ python/fetch/skswk9/baseparse.py
@@ -55,6 +55,28 @@ class BaseParse(threading.Thread):
 
         print '打开页面错误,重试3次还是错误', baseurl + url
         return BeautifulSoup('')
+    def fetchUrlWithBase(self, url):
+        count = 0
+        while count < maxCount:
+            try:
+                req = urllib2.Request(url, headers={
+                    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13', "Referer": "https://www.bbb670.com/htm/index.htm"})
+                req.encoding = 'utf-8'
+                response = urllib2.urlopen(req, timeout=300)
+                gzipped = response.headers.get(
+                    'Content-Encoding')  # 查看是否服务器是否支持gzip
+                content = response.read().decode('utf8', errors='replace')
+                if gzipped:
+                    content = zlib.decompress(
+                        content, 16 + zlib.MAX_WBITS)  # 解压缩，得到网页源码
+                soup = BeautifulSoup(content)
+                return soup
+            except Exception as e:
+                print '打开页面错误,重试', url, '次数', count
+                count = count + 1
+
+        print '打开页面错误,重试3次还是错误',  url
+        return BeautifulSoup('')
     def header(self):
     #         content = self.fetchContentUrl(headerUrl, header)
         content=''

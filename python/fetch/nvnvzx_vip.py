d2f2518ded828adc789e3c49efe82dd0fb7ad685
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-01-16 12:41:32 +0800

    111

--- python/fetch/nvnvzx_vip.py
+++ python/fetch/nvnvzx_vip.py
@@ -40,8 +40,8 @@ def parseVideo7():
 if __name__ == '__main__':
     ###parseVideo()
     #parseVideo2()
-    parseVideo3()
+    #parseVideo3()
 ###     #parseVideo4()
-    parseVideo5()
+    ###parseVideo5()
     parseVideo6()
     parseVideo7()

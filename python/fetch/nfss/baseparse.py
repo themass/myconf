037a9176a7d7eee952da30c06e92eb32ae41928d
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-01-17 14:23:34 +0800

    111

--- python/fetch/nfss/baseparse.py
+++ python/fetch/nfss/baseparse.py
@@ -15,7 +15,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 baseurl = "http://www.nfss.xyz"
 baseurl2 = "http://www.65aeae.com/"
-baseurl3 = "https://45mh.com/"
+baseurl3 = "http://www.mh40.xyz/"
 baseurl4 = "https://www.2448yy.com"
 header = {'User-Agent': 
           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": baseurl}

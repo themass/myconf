4d332ff05811342ab4b25220ad14732e90f16bad
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-01-24 22:18:08 +0800

    11

--- python/fetch/ff326/baseparse.py
+++ python/fetch/ff326/baseparse.py
@@ -11,7 +11,7 @@ import threading
 from BeautifulSoup import BeautifulSoup
 import re
 import os
-baseurl = "http://241yu.com"
+baseurl = "http://124fu.com"
 header = {'User-Agent':
           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', "Referer": baseurl}
 maxCount = 3
@@ -19,7 +19,7 @@ regVideo = re.compile(r"http(.*)m3u8'")
 iframeVideo = re.compile(r"videourl(.*)\.htm")
 playVideo = re.compile(r"varvHLSurl=(.*)\(\"(.*)\.m3u8")
 #http://www.561aa.com/htm/mp4list6/  是一家网站么？？？？？
-urlMap = {"javbox":"https://m3u8.299du.com/m3u8/javbox_water_m3u8_301","jav":"https://bojav.121yy.com/m3u8/jav-m3u8","kele":"https://bok9.121yy.com/m3u8/vod_water_m3u8/","lao":"https://bolao.121yy.com/m3u8"}
+urlMap = {"javbox":"https://m3u8.299du.com/m3u8/javbox_water_m3u8_500","jav":"https://bojav.121yy.com/m3u8/jav-m3u8","kele":"https://bok9.121yy.com/m3u8/vod_water_m3u8/","lao":"https://bolao.121yy.com/m3u8"}
 class BaseParse(threading.Thread):
 
     def __init__(self):

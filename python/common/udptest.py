17e1676eb876decdc6254e2123cbecdc499b4cdf
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-02-11 16:44:29 +0800

    11

--- python/common/udptest.py
+++ python/common/udptest.py
@@ -3,10 +3,8 @@ import os
 import time
 import socket
 import random
-#Code Time
 from datetime import datetime
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# 绑定 客户端口和地址:
 s.bind(('0.0.0.0', 3000))
 print 'Bind UDP on 9999...'
 while True:

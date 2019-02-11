03cffdfa8f987bc89ea483e81034be34629178b2
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-02-11 16:45:26 +0800

    11

--- python/common/udptest.py
+++ python/common/udptest.py
@@ -4,11 +4,12 @@ import time
 import socket
 import random
 from datetime import datetime
+sys.setdefaultencoding('utf8')
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('0.0.0.0', 3000))
 print 'Bind UDP on 9999...'
 while True:
-    # 接收数据 自动阻塞 等待客户端请求:
     data, addr = s.recvfrom(1024)
     print 'Received from %s:%s.' % addr
     s.sendto('Hello, %s!' % data, addr)

8a7bbe502e06360e342345241495a47390110145
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-02-23 23:26:36 +0800

    # WARNING: head commit changed in the meantime
    11111

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -87,7 +87,7 @@ def pase4():
     
 def pase5():
     
-    meiyke_vip.parseVideo()
+    #meiyke_vip.parseVideo()
     nfss_vip.parseVideo2()
     nfss_vip.parseVideo4()
     nfss_vip.parseVideo3()

213e53ec6953648337fd40500f4ee350a1db7d8e
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-03-14 17:49:53 +0800

    111

--- python/fetch/all_image_sync.py
+++ python/fetch/all_image_sync.py
@@ -37,8 +37,8 @@ if __name__ == '__main__':
 #     jjr128_vip.pareImg()
      
 #     xieegif_vip.parsewowantImg()
-    xieegif_vip.parserneihanImg()
-    xieegif_vip.parseratooImg()
+#     xieegif_vip.parserneihanImg()
+#     xieegif_vip.parseratooImg()
 #     xieegif_vip.parsehugao8Img()
     
 #     tt192_vip.parseImg()

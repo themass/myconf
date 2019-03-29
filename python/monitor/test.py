5a9412cd595754e1360a7cff1a95ecddcd5f7f41
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-03-30 02:57:30 +0800

    11

--- python/monitor/test.py
+++ python/monitor/test.py
@@ -20,7 +20,18 @@ def paresFile(name):
         for host in v:
             print "client %s{\nsecret        = FreeVPN@vpn5296\nshortname    = vpn_check\n}"%(host)
         print "#",k," end"
-        
+def paresFile2(name):      
+    data = ''
+    dataMap = []
+    with open(name, 'r') as f:
+        data = f.read()
+    obj = data.split("\n")
+    for item in obj:
+        if item.count(",")>=8:
+            items = item.split(",#")
+            print items[1]
+            dataMap.append(items[0])
+    print dataMap 
 if __name__ == '__main__':
-    paresFile('../txt/1003.txt')
+    paresFile2('../txt/score2.csv')
     

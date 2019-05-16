a11cb0001dd4e76493e34edbaa639dcd02078462
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-05-17 03:36:30 +0800

    11

--- python/fetch/se8/baseparse.py
+++ python/fetch/se8/baseparse.py
@@ -15,7 +15,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 #662cf,579cf,298cf
-baseurl = "https://www.69acy.com/"
+baseurl = "https://www.69any.com/"
 reg = re.compile(r"(.*\/)\d+\.htm")
 mp3Name = re.compile(r"<span>.*</span>")
 soundUrl = "/yousheng/index.html"

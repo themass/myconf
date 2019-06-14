354e35b2a8257aeec4b7e3e765be9f6f6ca568f1
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-06-15 02:59:28 +0800

    11

--- python/fetch/bx88222_vip.py
+++ python/fetch/bx88222_vip.py
@@ -9,7 +9,7 @@ from common import common
 from common import typeutil
 from common import db_ops
 from common import MyQueue
-from fff64 import *
+from bx88222 import *
 import re
 import sys
 reload(sys)

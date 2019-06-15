5c261b02e97723f0d0d92654854c4b1c5667859c
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-06-15 12:39:04 +0800

    11

--- python/fetch/tofile.py
+++ python/fetch/tofile.py
@@ -31,8 +31,8 @@ class ChannelFetch(threading.Thread):
         try:
             dbVPN = db.DbVPN()
             ops = db_ops.DbOps(dbVPN)
-            sortType = dateutil.y_m_d()
-#             sortType = "2019-06-02"
+#             sortType = dateutil.y_m_d()
+            sortType = "2019-06-14"
             for i in range(0, 20000):
                 #                 ret = ops.getTextChannelItems(self.t_item["url"], i)
                 ret = ops.getTextChannelItemsById(i, sortType)

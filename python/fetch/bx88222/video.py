89fa620c2f338d9489ca948e180f1076ab73a722
liguoqing19861028@163.com <liguoqing19861028@163.com>
2020-06-28 20:48:24 +0800

    11

--- python/fetch/bx88222/video.py
+++ python/fetch/bx88222/video.py
@@ -58,6 +58,8 @@ class VideoUserParse(BaseParse):
                 return False
             for li in lis:
                 ahref = li.first("a")
+                if ahref ==None:
+                    continue
                 obj = {}
                 mp4Url = self.parseDomVideo(ahref.get("href"))
                 if mp4Url == None:

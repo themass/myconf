a54f54bf95b769c810a5a9f7d96ad1b85e58a2af
liguoqing19861028@163.com <liguoqing19861028@163.com>
2020-07-03 20:31:13 +0800

    11

--- python/fetch/all_text_sync.py
+++ python/fetch/all_text_sync.py
@@ -13,25 +13,28 @@ import kpd36_vip
 from fetch import  bx88222_vip
 def pase1():
 #     novel941_vip.parseText()
-#     kedouwo_vip.parseText12()
-#     g6858_vip.parseText()
-#     ddd804_vip.parsetextddd804()
 #     ddd804_vip.parsetext65aeae()
     
-    kpd36_vip.parseText()
     bx88222_vip.parseText()
     sp878_vip.parseText()
+    kpd36_vip.parseText()
 def pase2():
     
-    se8_vip.startWork()
-    se8_vip.parseText()
+#     se8_vip.startWork()
+#     se8_vip.parseText()
     novel941_vip.parseText2()
     ####mayi01_vip.parseText()
+def pase3():
+    g6858_vip.parseText()
+    ddd804_vip.parsetextddd804()
+    kedouwo_vip.parseText12()
 if __name__ == '__main__':
     val = argsMap.get("-p",0)
     if int(val)==1:
         pase1()
     elif int(val)==2:
         pase2()
+    elif int(val)==3:
+        pase3()
     
     

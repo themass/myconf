8787d30596a1f5fa4310b40daf5e38ad6b3496f8
liguoqing19861028@163.com <liguoqing19861028@163.com>
2020-07-04 17:21:29 +0800

    11

--- python/fetch/xmhxmygs/baseparse.py
+++ python/fetch/xmhxmygs/baseparse.py
@@ -77,7 +77,7 @@ class BaseParse(threading.Thread):
 #         content = self.fetchContentUrl(headerUrl, header)
         content=''
         print "os.path.dirname(os.path.realpath(__file__))=%s" % os.path.dirname(os.path.realpath(__file__)) 
-        with open("av8/%s"%(name)) as f:
+        with open("xmhxmygs/%s"%(name)) as f:
             for line in f.readlines():
                 content = "%s%s"%(content,line)
         soup= BeautifulSoup(content)

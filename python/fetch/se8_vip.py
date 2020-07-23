5ca552788d1ccaa0305d04a2a3c40e08dbc1205b
themass <liguoqing19861028@163.com>
2020-07-24 03:01:44 +0800

    11

--- python/fetch/se8_vip.py
+++ python/fetch/se8_vip.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
     
     #     options, args = getopt.getopt(sys.argv[1:], "s:t:i:g")
     parseVideo()
-#     parseSound()
+    parseSound()
 #     parseGirlImg()
 #     parseImg()
 #     parseText()

7ed5eb05f16e2eb293f26c13f80ff9c92fa6b8ac
themass <liguoqing19861028@163.com>
2020-07-24 02:59:02 +0800

    11

--- python/fetch/se8/baseparse.py
+++ python/fetch/se8/baseparse.py
@@ -15,7 +15,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 #662cf,579cf,298cf,v5c5
-baseurl = "https://www.a55d4593a3b1.com/"
+baseurl = "https://www.cb3b4969244f.com/"
 reg = re.compile(r"(.*\/)\d+\.htm")
 mp3Name = re.compile(r"<span>.*</span>")
 soundUrl = "/yousheng/index.html"

9131530f959c5c1992a876dbc921d6008cf3ddf3
themass <liguoqing19861028@163.com>
2020-08-01 00:55:46 +0800

    11

---
+++ python/fetch/duorydonng/video.py
@@ -0,0 +1,106 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+from baseparse import *
+from urlparse import urlparse
+from common import common
+from urllib import unquote
+import time
+from fetch.profile import *
+class VideoUserParse(BaseParse):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        chs = self.videoChannel()
+        for item in chs:
+            ops.inertVideoUser(item)
+        print 'duorydonng user video -- channel ok;,len=',len(chs)
+        dbVPN.commit()
+        dbVPN.close()
+        for item in chs:
+            for i in range(1, maxVideoPage):
+                url= item['url']
+                url = "%s*%s"%(item['url'],i)
+                print url
+                con = self.videoParse(item['channel'], url,item['userId'])
+                if con==False:
+                    print '没有数据了啊-======页数',i,'---',item['name'],item['url']
+                    break
+                print '解析完成 ', item['channel'], ' ---', i, '页'
+    def videoChannel(self):
+        ahrefs = self.header()
+        channelList = []
+        for ahref in ahrefs:
+            obj={}
+            obj['name']=ahref.text
+            obj['url']=ahref.get('href')
+            obj['baseurl']=baseurl
+            obj['updateTime']=datetime.datetime.now()
+            obj['pic']=''
+            obj['rate']=1.2
+            obj['channel']='duorydonng'
+            obj['userId']="duorydonng_"+ahref.text
+            obj['showType']=3
+            obj['channelType']='normal'
+            channelList.append(obj)
+        return channelList
+    def videoParse(self, channel, url,userId):
+        dataList = []
+        soup = self.fetchUrl(url)
+        div = soup.first("div",{"class":"cards video-list"})
+        if div!=None:
+            lis = div.findAll("div",{"class":"col-md-2 col-xs-4"})
+            if len(lis)==0:
+                return False
+            for li in lis:
+                ahref = li.first("a")
+                if ahref ==None:
+                    continue
+                obj = {}
+                mp4Url = self.parseDomVideo(ahref.get("href"))
+                if mp4Url == None:
+                    print '没有mp4 文件:', ahref.get("href")
+                    continue
+                obj['url'] = mp4Url
+                img = li.first("img")
+                obj['pic'] = "https"+img.get("src")
+                obj['name'] = li.first('strong').text
+    
+                videourl = urlparse(obj['url'])
+                obj['path'] = "duorydonng"+videourl.path
+                obj['rate'] = 1.2
+                obj['updateTime'] = datetime.datetime.now() 
+                obj['userId'] = userId
+                obj['baseUrl'] = baseurl
+                obj['showType'] = 3
+                if mp4Url.count("m3u8")==0 and mp4Url.count("mp4")==0:
+                    obj['videoType'] = "webview"
+                else:
+                    obj['videoType'] = "normal"
+                print obj['videoType'],obj['name'],mp4Url,obj['pic']
+                dataList.append(obj)
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        for obj in dataList:
+            ops.inertVideoUserItem(obj)
+
+        print '8x8x video --解析完毕 ; channel =', channel, '; len=', len(dataList), url
+        dbVPN.commit()
+        dbVPN.close()
+        return True
+    def parseDomVideo(self, url):
+        try:
+            url = url.replace("video_detail","video_conter")
+            soup = self.fetchUrl(url)
+            iframe = soup.first("iframe")
+            if iframe!=None:
+                return iframe.get("src").replace("/Static/Home/VideoJS/?Play=","")
+            print url,'没有mp4'
+            return None
+        except Exception as e:
+            print common.format_exception(e)
+            return None
+

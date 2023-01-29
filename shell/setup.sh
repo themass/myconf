54f5fb80c41059c5118300ccb735c4a417672111
liguoqing06 <liguoqing06@kuaishou.com>
2023-01-29 20:12:19 +0800

    sample

--- shell/setup.sh
+++ shell/setup.sh
@@ -3,92 +3,14 @@
 ## Version setting
 ## -----------------------
 
-URL=http://10.134.3.8/software
-APP_HOME=/home/work/local
-APP_DW_HOME=/home/software/tmp
-
-JDK_VERSION=jdk1.7.0_80
-
-MAVEN_VERSION=apache-maven-3.0.5
-
-TOMCAT_VERSION=tomcat_vpn
-
-REDIS_VERSION=redis-3.2.3
+APP_HOME=/root/local
+TMP_HOME=/root/soft
 
 MYSQL_VERSION=mysql-server_5.7.14-1ubuntu14.04_amd64.deb-bundle
 
-NGINX_VERSION=nginx-1.10.1
-NGINX_REDIS_VERSION=ngx_http_redis-0.3.7
-NGINX_REDIS2_VERSION=redis2-nginx-module-0.11
-NGINX_SRCACHE_VERSION=srcache-nginx-module-0.26
-NGINX_DEVEL_VERSION=ngx_devel_kit-0.2.19
-NGINX_MISC_VERSION=set-misc-nginx-module-0.24
-NGINX_SLOWFS_VERSION=ngx_slowfs_cache-1.10
-NGINX_PURGE_VERSION=ngx_cache_purge-2.1
-NGINX_PHP_VERSION=ngx_http_php_session-0.3
-NGINX_ACCOUNTING_VERSION=ngx_http_accounting_module-0.2
-NGINX_CONSISTENT_VERSION=ngx_http_consistent_hash
-NGINX_ECHO_VERSION=echo-nginx-module
-NGINX_PCRE_VERSION=pcre-8.39
-
-mkdir -p ${APP_HOME}
-mkdir -p ${APP_HOME}
-
-## -----------------------
-## Setup JDK
-## -----------------------
-setup_jdk() {
-	sudo add-apt-repository ppa:webupd8team/java
-	sudo apt-get update
-	sudo apt-get install oracle-java8-installer
-	sudo apt-get install oracle-java8-set-default
-	sudo sudo update-java-alternatives -s java-8-oracle
-	sudo java -version
-	sudo which java
-	#　update-alternatives --config java
-	#修改path 
-	#    cd ${APP_HOME}
-	#    rm -f ${APP_DW_HOME}/${JDK_VERSION}*
-	#    rm -rf ${JDK_VERSION}
-	#    wget ${URL}/jdk/${JDK_VERSION}.tar.gz -O  ${APP_DW_HOME}/${JDK_VERSION}.tar.gz
-	#    tar -zxvf ${APP_DW_HOME}/${JDK_VERSION}.tar.gz 
-	#    echo "yes" | ${APP_HOME}/${JDK_VERSION} | cat
-	#    rm -rf ${APP_HOME}/jdk
-	#    ln -s ${APP_HOME}/${JDK_VERSION} ${APP_HOME}/jdk
-
-}
-
-## -----------------------
-## Setup maven
-## -----------------------
-setup_maven() {
-	#sudo apt-get install maven2
-    cd ${APP_HOME}
-    rm -f ${APP_DW_HOME}/${MAVEN_VERSION}-bin.tar.gz
-    rm -rf ${MAVEN_VERSION}
-    wget ${URL}/maven/${MAVEN_VERSION}-bin.tar.gz -O ${APP_DW_HOME}/${MAVEN_VERSION}-bin.tar.gz
-    tar zxvpf ${APP_DW_HOME}/${MAVEN_VERSION}-bin.tar.gz
-     rm -rf ${APP_HOME}/maven
-    ln -s ${APP_HOME}/${MAVEN_VERSION} ${APP_HOME}/maven
-    #wget ${URL}/setup/conf/maven/settings.xml -O ${APP_HOME}/maven/conf/settings.xml
-}
-
-## -----------------------
-## Setup tomcat
-## -----------------------
-setup_tomcat() {
-#	useradd -r -m -s /bin/bash work 
-#	su -work
-#	alias rtm='ps -aux| grep tomcat_vpn |awk '\''{print$2}'\''|xargs kill -9 ;/home/work/local/tomcat_vpn/bin/startup.sh'
-#	alias vpu='cd /home/work/webroot/vpn-api ;ps -aux| grep tomcat_vpn |awk '\''{print$2}'\''|xargs kill -9 ;mvn clean;git pull;mvn install -Ppro; rtm;'
-#	
-	
- 	cd ${APP_HOME}
-    rm -f ${APP_DW_HOME}/${TOMCAT_VERSION}.tar.bz2
-    rm -rf ${TOMCAT_VERSION}
-    wget ${URL}/tomcat/${TOMCAT_VERSION}.tar.bz2 -O ${APP_DW_HOME}/${TOMCAT_VERSION}.tar.bz2
-    tar -jxvf ${APP_DW_HOME}/${TOMCAT_VERSION}.tar.bz2
-}
+mkdir -p /root/soft
+mkdir -p /root/local
+mkdir -p /root/work
 
 ## -----------------------
 ## Setup MySQL
@@ -164,123 +86,53 @@ setup_user() {
 }
 
 ## -----------------------
-## Setup redis
-## -----------------------
-setup_redis() {
- 	cd ${APP_DW_HOME}
-    rm -f ${APP_DW_HOME}/${REDIS_VERSION}.tar.bz2
-    rm -rf ${REDIS_VERSION}
-    rm -rf ${APP_HOME}/${REDIS_VERSION}
-    rm -rf ${APP_HOME}/redis
-    wget ${URL}/store/${REDIS_VERSION}.tar.gz -O ${APP_DW_HOME}/${REDIS_VERSION}.tar.gz
-    tar -zxvf ${APP_DW_HOME}/${REDIS_VERSION}.tar.gz
-    cd ${APP_DW_HOME}/${REDIS_VERSION}	
-    make -j4
-    mkdir -p ${APP_HOME}/${REDIS_VERSION}/bin 
-    cp ${APP_DW_HOME}/${REDIS_VERSION}/src/redis*  ${APP_HOME}/${REDIS_VERSION}/bin/
-    rm ${APP_HOME}/${REDIS_VERSION}/bin/*.o  ${APP_HOME}/${REDIS_VERSION}/bin/*.c ${APP_HOME}/${REDIS_VERSION}/bin/*.h
-     
- 	ln -s ${APP_HOME}/${REDIS_VERSION} ${APP_HOME}/redis 
- 	wget ${URL}/setup/myconf/redis/redis.conf -O ${APP_HOME}/redis/redis.conf
- 	${APP_HOME}/redis/bin/redis-server  ${APP_HOME}/redis/redis.conf
- 	
-}
-
-## -----------------------
 ## Setup nginx
 ## -----------------------
 setup_nginx() {
-    cd ${APP_DW_HOME}
-    rm -f ${NGINX_VERSION}.tar.gz
-    rm -rf ${NGINX_VERSION}
-    rm -rf ${APP_HOME}/${NGINX_VERSION}
-    wget ${URL}/nginx/${NGINX_VERSION}.tar.gz
-    tar zxvf ${NGINX_VERSION}.tar.gz
+    cd ${TMP_HOME}
+    rm -rf openssl
+    git clone https://github.com/openssl/openssl
+    cd openssl/
+    ./configure --with-openssl-includes=/root/soft/openssl --with-openssl-libraries=/root/soft/openssl
 
-	rm -f ${NGINX_REDIS_VERSION}.tar.gz
-    rm -rf ${NGINX_REDIS_VERSION}
-    wget ${URL}/nginx/${NGINX_REDIS_VERSION}.tar.gz
-    tar zxvf ${NGINX_REDIS_VERSION}.tar.gz
-    
-    rm -f ${NGINX_REDIS2_VERSION}.tar.gz
-    rm -rf ${NGINX_REDIS2_VERSION}
-    wget ${URL}/nginx/${NGINX_REDIS2_VERSION}.tar.gz
-    tar zxvf ${NGINX_REDIS2_VERSION}.tar.gz
-    
-    rm -f ${NGINX_SRCACHE_VERSION}.tar.gz
-    rm -rf ${NGINX_SRCACHE_VERSION}
-    wget ${URL}/nginx/${NGINX_SRCACHE_VERSION}.tar.gz
-    tar zxvf ${NGINX_SRCACHE_VERSION}.tar.gz
-    
-    rm -f ${NGINX_DEVEL_VERSION}.tar.gz
-    rm -rf ${NGINX_DEVEL_VERSION}
-    wget ${URL}/nginx/${NGINX_DEVEL_VERSION}.tar.gz
-    tar zxvf ${NGINX_DEVEL_VERSION}.tar.gz
-    
-    rm -f ${NGINX_MISC_VERSION}.tar.gz
-    rm -rf ${NGINX_MISC_VERSION}
-    wget ${URL}/nginx/${NGINX_MISC_VERSION}.tar.gz
-    tar zxvf ${NGINX_MISC_VERSION}.tar.gz
-    
-    rm -f ${NGINX_SLOWFS_VERSION}.tar.gz
-    rm -rf ${NGINX_SLOWFS_VERSION}
-    wget ${URL}/nginx/${NGINX_SLOWFS_VERSION}.tar.gz
-    tar zxvf ${NGINX_SLOWFS_VERSION}.tar.gz
-    
-    rm -f ${NGINX_PURGE_VERSION}.tar.gz
-    rm -rf ${NGINX_PURGE_VERSION}
-    wget ${URL}/nginx/${NGINX_PURGE_VERSION}.tar.gz
-    tar zxvf ${NGINX_PURGE_VERSION}.tar.gz
-    
-    rm -f ${NGINX_PHP_VERSION}.tar.gz
-    rm -rf ${NGINX_PHP_VERSION}
-    wget ${URL}/nginx/${NGINX_PHP_VERSION}.tar.gz
-    tar zxvf ${NGINX_PHP_VERSION}.tar.gz
-    
-    rm -f ${NGINX_ACCOUNTING_VERSION}.tar.gz
-    rm -rf ${NGINX_ACCOUNTING_VERSION}
-    wget ${URL}/nginx/${NGINX_ACCOUNTING_VERSION}.tar.gz
-    tar zxvf ${NGINX_ACCOUNTING_VERSION}.tar.gz
-    
-    rm -f ${NGINX_PCRE_VERSION}.zip
-    rm -rf ${NGINX_PCRE_VERSION}
-    wget ${URL}/nginx/${NGINX_PCRE_VERSION}.zip
-    unzip ${NGINX_PCRE_VERSION}.zip
-    
-    rm -rf ${NGINX_ECHO_VERSION}
-    git clone git://github.com/agentzh/echo-nginx-module.git
+    rm pcre-8.39.tar.gz
+    wget https://onboardcloud.dl.sourceforge.net/project/pcre/pcre/8.39/pcre-8.39.tar.gz --no-check-certificate
+    tar -zxvf pcre-8.39.tar.gz
 
-    rm -rf ${NGINX_CONSISTENT_HASH_VERSION}
-    git clone https://github.com/replay/ngx_http_consistent_hash.git
-
-	
-    cd ${NGINX_VERSION}
-	./configure --prefix=${APP_HOME}/${NGINX_VERSION} --with-http_stub_status_module --with-http_ssl_module --with-http_gzip_static_module \
-	--with-pcre=../${NGINX_PCRE_VERSION}
-	--add-module=../${NGINX_REDIS_VERSION} --add-module=../${NGINX_REDIS2_VERSION} --add-module=../${NGINX_SRCACHE_VERSION} \
-	--add-module=../${NGINX_DEVEL_VERSION} --add-module=../${NGINX_PURGE_VERSION} --add-module=../${NGINX_SLOWFS_VERSION=} \
-	--add-module=../${NGINX_PHP_VERSION} --add-module=../${NGINX_ACCOUNTING_VERSION} --add-module=../${NGINX_CONSISTENT_VERSION}  \
-	--add-module=../${NGINX_CONSISTENT_VERSION} --add-module=../${NGINX_ECHO_VERSION}
-	make -j4
+    rm -rf nginx-1.16.1.tar.gz
+    rm -rf nginx-1.16.1
+    wget http://nginx.org/download/nginx-1.16.1.tar.gz
+    git clone https://github.com/openresty/redis2-nginx-module
+    git clone https://github.com/openresty/srcache-nginx-module
+    git clone https://github.com/openresty/set-misc-nginx-module
+    git clone https://github.com/openresty/echo-nginx-module
+    wget https://github.com/vision5/ngx_devel_kit/archive/refs/tags/v0.3.1.tar.gz --no-check-certificate
+    git clone https://github.com/FRiCKLE/ngx_cache_purge
+    git clone https://github.com/replay/ngx_http_consistent_hash
+    git clone https://github.com/replay/ngx_http_php_session
+    git clone https://github.com/Yongke/ngx_http_redis-0.3.7
+    git clone http://github.com/FRiCKLE/ngx_slowfs_cache/
+    tar -zxvf v0.3.1.tar.gz
+    tar -zxvf nginx-1.16.1.tar.gz
+    cd nginx-1.16.1/
+    ./configure --prefix=/root/local/nginx-1.16.1 --with-http_stub_status_module  --with-cc-opt="-Wimplicit-fallthrough=0" --with-http_gzip_static_module --with-pcre=../pcre-8.39 --add-module=../redis2-nginx-module --add-module=../srcache-nginx-module --add-module=../ngx_devel_kit-0.3.1 --add-module=../ngx_cache_purge --add-module=../ngx_http_consistent_hash --add-module=../ngx_http_php_session --add-module=../ngx_slowfs_cache   --add-module=../set-misc-nginx-module
+#    --with-http_ssl_module --with-openssl=/root/soft  --add-module=../ngx_http_redis-0.3.7
+    make
     make install
-    rm -rf ${APP_HOME}/nginx
-    ln -s ${APP_HOME}/${NGINX_VERSION} ${APP_HOME}/nginx
-    wget ${URL}/setup/myconf/nginx/conf/nginx.conf -O ${APP_HOME}/nginx/conf/nginx.conf
-    rm -rf ${APP_HOME}/nginx/html
-}
+    mkdir -p /root/local/nginx-11.16.1/logs/
+    rm -rf /root/local/nginx
+    ln -s /root/local/nginx-1.16.1 /root/local/nginx
+    rm /root/local/nginx/conf/nginx.conf
 
-## -----------------------
-## Setup all aboves
-## -----------------------
-setup_all() {
-    setup_jdk
-    setup_maven
-    setup_nginx
-    setup_mysql
-    setup_mongodb
-    setup_python
-    setup_rsync
-    setup_tomcat
+    mkdir -p /home/file/nginx/cache/temp
+    mkdir -p /home/file/nginx_cache/cache/body
+    cd /root/work/nginx/conf/
+    mkdir server
+    cp /root/work/myconf/nginx-sample/nginx.conf  .
+    cp /root/work/myconf/nginx-sample/nginx_check.conf  server/.
+    cp /root/work/myconf/nginx-sample/upstream.conf server/.
+    cp /root/work/myconf/nginx-sample/vpn.conf  server/.
+    cd
 }
 
 ## -----------------------
@@ -308,18 +160,8 @@ usage() {
 if [ $# != 0 ]; then
     for arg in $*; do
         case "$arg" in
-            jdk)            setup_jdk;;
             nginx)          setup_nginx;;
-	    	OpenResty)      setup_openresty;;
-            memcached)      setup_memcached;;
-	    	redis)	    setup_redis;;
-	    	tomcat)	    setup_tomcat;;
-	    	mysql)	    setup_mysql;;
-            mongodb)        setup_mongodb;;
-            python)         setup_python;;
-            rsync)          setup_rsync;;
-            maven)          setup_maven;;
-            all)            setup_all;;
+	    	    mysql)	    setup_mysql;;
             *)              usage;;
         esac
     done
--- shell/soft_setup_web.sh
+++ shell/soft_setup_web.sh
@@ -2,302 +2,71 @@
 ## -----------------------
 ## Version setting
 ## -----------------------
-URL=http://file.sspacee.com
-APP_HOME=/home/web/local
-WEB_HOME=/home/web/webroot
-TMP_HOME=/home/web/soft
-JDK_VERSION=jdk-8u181-linux-x64
-JDK_DIR=jdk1.8
-
-SDK_NAME_VERSION=android-sdk_r24.4.1-linux
-SDK_VERSION=android-sdk-linux
-SDK_PLATFORM_VERSION=platform-tools_r23.1.0-linux
-
-SDK_PLATFORM_P_VERSION_1=android-17_r01
-SDK_PLATFORM_P_VERSION_2=android-19_r03
-SDK_PLATFORM_P_VERSION_3=android-21_r01
-SDK_PLATFORM_P_VERSION_4=android-22_r02
-SDK_PLATFORM_P_VERSION_5=android-23_r02
-
-SDK_PLATFORM_P_VERSION_P_1=android-4.2
-SDK_PLATFORM_P_VERSION_P_2=android-4.4.2
-SDK_PLATFORM_P_VERSION_P_3=android-5.0
-SDK_PLATFORM_P_VERSION_P_4=android-5.1.1
-SDK_PLATFORM_P_VERSION_P_5=android-6.0
-
-SDK_PLATFORM_P_VERSION_P_P_1=android-17x
-SDK_PLATFORM_P_VERSION_P_P_2=android-19
-SDK_PLATFORM_P_VERSION_P_P_3=android-21
-SDK_PLATFORM_P_VERSION_P_P_4=android-22
-SDK_PLATFORM_P_VERSION_P_P_5=android-23
-
-BUILD_TOOL_P_VERSION_1=build-tools_r19.1
-BUILD_TOOL_P_VERSION_2=build-tools_r22.0.1
-BUILD_TOOL_P_VERSION_3=build-tools_r23.0.3
-BUILD_TOOL_P_VERSION_4=build-tools_r25.0.2
-
-BUILD_TOOL_P_VERSION_P_1=android-4.4.2 
-BUILD_TOOL_P_VERSION_P_2=android-5.1
-BUILD_TOOL_P_VERSION_P_3=android-6.0
-BUILD_TOOL_P_VERSION_P_4=android-7.1.1
-
-BUILD_TOOL_P_VERSION_P_P_1=19.1
-BUILD_TOOL_P_VERSION_P_P_2=22.0.1
-BUILD_TOOL_P_VERSION_P_P_3=23.0.3
-BUILD_TOOL_P_VERSION_P_P_4=25.0.2
-
-NDK_VERSION=android-ndk-r14b-linux-x86_64
-MAVEN_VERSION=apache-maven-3.0.3
-NGINX_VERSION=nginx-1.10.2
-NGINX_REDIS_VERSION=ngx_http_redis-0.3.7
-NGINX_REDIS2_VERSION=redis2-nginx-module-0.11
-NGINX_SRCACHE_VERSION=srcache-nginx-module-0.26
-NGINX_DEVEL_VERSION=ngx_devel_kit-0.2.19
-NGINX_MISC_VERSION=set-misc-nginx-module-0.24
-NGINX_SLOWFS_VERSION=ngx_slowfs_cache-1.10
-NGINX_PURGE_VERSION=ngx_cache_purge
-NGINX_PHP_VERSION=ngx_http_php_session-0.3
-NGINX_ACCOUNTING_VERSION=ngx_http_accounting_module-0.2
-NGINX_CONSISTENT_VERSION=ngx_http_consistent_hash
-NGINX_ECHO_VERSION=echo-nginx-module
-NGINX_PCRE_VERSION=pcre-8.39
 
 MYSQL_VERSION=mysql-5.6.34-linux-glibc2.5-x86_64
 RADIUS_VERSION=freeradius-server-2.1.12
 
 PHP_VERSION=php-7.1.12
-
 REDIS_VERSION=redis-3.2.6
-MAVEN_VERSION=apache-maven-3.3.9-bin
-mkdir -p ${APP_HOME}
-mkdir -p ${TMP_HOME}
+
 PWD=`pwd`
 
+APP_HOME=/home/web/local
+WEB_HOME=/home/web/webroot
+TMP_HOME=/home/web/soft
+
+mkdir -p /home/web/local
+mkdir -p /home/web/soft
+mkdir -p /home/web/work
 #init soft
-setup_jdk() 
-{
-	cd ${APP_HOME}
-    rm -f ${TMP_HOME}/${JDK_VERSION}.tar.gz
-    rm -rf ${JDK_DIR}
-    wget ${URL}/soft/${JDK_VERSION}.tar.gz -O ${TMP_HOME}/${JDK_VERSION}.tar.gz
-    tar -zxvf ${TMP_HOME}/${JDK_VERSION}.tar.gz 
-	echo 'profile' ${APP_HOME}/${JDK_VERSION}
-	mkdir -p /home/web/var/log/ 
-	mkdir -p /home/web/var/run/
-	mkdir -p /home/web/var/log/vpn/
+## -----------------------
+## Setup JDK
+## -----------------------
+setup_jdk() {
+  #https://www.oracle.com/java/technologies/javase/javase8u211-later-archive-downloads.html
+  #mvn clean install -Ppro
+  wget https://download.oracle.com/java/18/archive/jdk-18.0.2.1_linux-x64_bin.tar.gz --no-check-certificate
+  tar -zxvf jdk-18.0.2.1_linux-x64_bin.tar.gz
+  cd /home/web/local
+  rm -rf jdk-18.0.2.1
+  mv ../soft/jdk-18.0.2.1 .
+  echo "。profile and java - version"
+
+
+#	sudo add-apt-repository ppa:webupd8team/java
+#	sudo apt-get update
+#	sudo apt-get install oracle-java8-installer
+#	sudo apt-get install oracle-java8-set-default
+#	sudo sudo update-java-alternatives -s java-8-oracle
+#	sudo java -version
+#	sudo which java
+	#　update-alternatives --config java
+	#修改path
+
 }
-setup_sdk() 
-{
-	cd ${APP_HOME}
-	rm -f ${TMP_HOME}/${SDK_VERSION}.tgz
-	rm -rf ${SDK_VERSION}
-	
-	wget ${URL}/soft/${SDK_NAME_VERSION}.tgz -O ${TMP_HOME}/${SDK_VERSION}.tgz
-	#cp /home/web/work/${SDK_NAME_VERSION}.tgz  ${TMP_HOME}/${SDK_VERSION}.tgz
-	tar -zxvf ${TMP_HOME}/${SDK_VERSION}.tgz 
-	
-	rm -rf ${TMP_HOME}/${SDK_PLATFORM_VERSION}.zip 
-	wget ${URL}/soft/${SDK_PLATFORM_VERSION}.zip -O ${TMP_HOME}/${SDK_PLATFORM_VERSION}.zip
-	#cp /home/web/work/${SDK_PLATFORM_VERSION}.zip  ${TMP_HOME}/${SDK_PLATFORM_VERSION}.zip
-	unzip ${TMP_HOME}/${SDK_PLATFORM_VERSION}.zip
-	mv platform-tools  ${SDK_VERSION}/
-	
-	init_plateform
-	init_buildtool
-	echo 'profile' ${APP_HOME}/${SDK_VERSION} 
-	
-}
-init_buildtool(){
-	mkdir -p ${SDK_VERSION}/build-tools
-	rm -f ${TMP_HOME}/${BUILD_TOOL_P_VERSION_1}-linux.zip
-	rm -f ${TMP_HOME}/${BUILD_TOOL_P_VERSION_2}-linux.zip
-	rm -f ${TMP_HOME}/${BUILD_TOOL_P_VERSION_3}-linux.zip
-	rm -f ${TMP_HOME}/${BUILD_TOOL_P_VERSION_4}-linux.zip
-	
-	wget ${URL}/soft/${BUILD_TOOL_P_VERSION_1}-linux.zip -O ${TMP_HOME}/${BUILD_TOOL_P_VERSION_1}.zip
-	wget ${URL}/soft/${BUILD_TOOL_P_VERSION_2}-linux.zip -O ${TMP_HOME}/${BUILD_TOOL_P_VERSION_2}.zip
-	wget ${URL}/soft/${BUILD_TOOL_P_VERSION_3}-linux.zip -O ${TMP_HOME}/${BUILD_TOOL_P_VERSION_3}.zip
-	wget ${URL}/soft/${BUILD_TOOL_P_VERSION_4}-linux.zip -O ${TMP_HOME}/${BUILD_TOOL_P_VERSION_4}.zip
-	
-	#cp /home/web/work/${BUILD_TOOL_P_VERSION_1}-linux.zip ${TMP_HOME}/${BUILD_TOOL_P_VERSION_1}.zip
-	#cp /home/web/work/${BUILD_TOOL_P_VERSION_2}-linux.zip ${TMP_HOME}/${BUILD_TOOL_P_VERSION_2}.zip
-	#cp /home/web/work/${BUILD_TOOL_P_VERSION_3}-linux.zip ${TMP_HOME}/${BUILD_TOOL_P_VERSION_3}.zip
-	#cp /home/web/work/${BUILD_TOOL_P_VERSION_4}-linux.zip ${TMP_HOME}/${BUILD_TOOL_P_VERSION_4}.zip
-	
-	unzip ${TMP_HOME}/${BUILD_TOOL_P_VERSION_1}.zip  
-	unzip ${TMP_HOME}/${BUILD_TOOL_P_VERSION_2}.zip  
-	unzip ${TMP_HOME}/${BUILD_TOOL_P_VERSION_3}.zip  
-	unzip ${TMP_HOME}/${BUILD_TOOL_P_VERSION_4}.zip  
-	
-	mv ${BUILD_TOOL_P_VERSION_P_1} ${SDK_VERSION}/build-tools/${BUILD_TOOL_P_VERSION_P_P_1}
-	mv ${BUILD_TOOL_P_VERSION_P_2} ${SDK_VERSION}/build-tools/${BUILD_TOOL_P_VERSION_P_P_2}
-	mv ${BUILD_TOOL_P_VERSION_P_3} ${SDK_VERSION}/build-tools/${BUILD_TOOL_P_VERSION_P_P_3}
-	mv ${BUILD_TOOL_P_VERSION_P_4} ${SDK_VERSION}/build-tools/${BUILD_TOOL_P_VERSION_P_P_4}
-}
-init_plateform(){
-	mkdir -p ${SDK_VERSION}/platforms
-	rm -f ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_1}.zip
-	rm -f ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_2}.zip
-	rm -f ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_3}.zip
-	rm -f ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_4}.zip
-	rm -f ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_5}.zip
-	
-	wget ${URL}/soft/${SDK_PLATFORM_P_VERSION_1}.zip -O ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_1}.zip
-	wget ${URL}/soft/${SDK_PLATFORM_P_VERSION_2}.zip -O ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_2}.zip
-	wget ${URL}/soft/${SDK_PLATFORM_P_VERSION_3}.zip -O ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_3}.zip
-	wget ${URL}/soft/${SDK_PLATFORM_P_VERSION_4}.zip -O ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_4}.zip
-	wget ${URL}/soft/${SDK_PLATFORM_P_VERSION_5}.zip -O ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_5}.zip
-	
-	#cp /home/web/work/${SDK_PLATFORM_P_VERSION_1}.zip  ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_1}.zip
-	#cp /home/web/work/${SDK_PLATFORM_P_VERSION_2}.zip  ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_2}.zip
-	#cp /home/web/work/${SDK_PLATFORM_P_VERSION_3}.zip  ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_3}.zip
-	#cp /home/web/work/${SDK_PLATFORM_P_VERSION_4}.zip  ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_4}.zip
-	#cp /home/web/work/${SDK_PLATFORM_P_VERSION_5}.zip  ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_5}.zip
-	
-	
-	unzip ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_1}.zip  
-	unzip ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_2}.zip  
-	unzip ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_3}.zip  
-	unzip ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_4}.zip  
-	unzip ${TMP_HOME}/${SDK_PLATFORM_P_VERSION_5}.zip  
-	
-	mv ${SDK_PLATFORM_P_VERSION_P_1} ${SDK_VERSION}/platforms/${SDK_PLATFORM_P_VERSION_P_P_1}
-	mv ${SDK_PLATFORM_P_VERSION_P_2} ${SDK_VERSION}/platforms/${SDK_PLATFORM_P_VERSION_P_P_2}
-	mv ${SDK_PLATFORM_P_VERSION_P_3} ${SDK_VERSION}/platforms/${SDK_PLATFORM_P_VERSION_P_P_3}
-	mv ${SDK_PLATFORM_P_VERSION_P_4} ${SDK_VERSION}/platforms/${SDK_PLATFORM_P_VERSION_P_P_4}
-	mv ${SDK_PLATFORM_P_VERSION_P_5} ${SDK_VERSION}/platforms/${SDK_PLATFORM_P_VERSION_P_P_5}
-}
-setup_ndk() 	
-{
-	cd ${APP_HOME}
-	rm -f ${TMP_HOME}/${NDK_VERSION}.zip
-	rm -rf ${NDK_VERSION}
-	wget ${URL}/soft/${NDK_VERSION}.zip -O ${TMP_HOME}/${NDK_VERSION}.zip
-	unzip ${TMP_HOME}/${NDK_VERSION}.zip
-	echo 'profile' ${APP_HOME}/${NDK_VERSION}
-}
+
 setup_maven(){
-	cd ${APP_HOME}
-	rm -rf ${MAVEN_VERSION}
-	wget ${URL}/soft/${MAVEN_VERSION}.tar.gz -O ${TMP_HOME}/${MAVEN_VERSION}.tar.gz
-	tar -zxvf ${TMP_HOME}/${MAVEN_VERSION}.tar.gz 
-	echo 'profile ' ${APP_HOME}/${MAVEN_VERSION}/bin
+  cd /home/web/soft
+  wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz --no-check-certificate
+  tar -zxvf apache-maven-3.8.7-bin.tar.gz
+  cd cd /home/web/local
+   mv ../soft/apache-maven-3.8.7 .
+   echo " mod .profile"
+
 }
 setup_redis() 
 {
-#http://download.redis.io/releases/redis-4.0.10.tar.gz	
-	shelldir=`pwd`
-	cd ${TMP_HOME}
-	rm -rf ${APP_HOME}/${REDIS_VERSION}
-    rm -f ${TMP_HOME}/${REDIS_VERSION}.tar.gz
-    wget ${URL}/soft/${REDIS_VERSION}.tar.gz 
-    tar -zxvf ${REDIS_VERSION}.tar.gz 
-    cd ${REDIS_VERSION}
-    make
-    make PREFIX=${APP_HOME}/${REDIS_VERSION} install
-    cp ${shelldir}/../redis/redis.conf  ${APP_HOME}/${REDIS_VERSION}/
-	echo 'profile' ${APP_HOME}/${REDIS_VERSION}
-}
-setup_nginx() {
-	shelldir=`pwd`
-	cd ${TMP_HOME}
-	
-	rm -f ${NGINX_VERSION}.tar.gz
-	rm -f ${NGINX_REDIS_VERSION}.tar.gz
-    rm -f ${NGINX_REDIS2_VERSION}.tar.gz
-    rm -f ${NGINX_SRCACHE_VERSION}.tar.gz
-    rm -f ${NGINX_DEVEL_VERSION}.tar.gz
-    rm -f ${NGINX_MISC_VERSION}.tar.gz
-    rm -f ${NGINX_SLOWFS_VERSION}.tar.gz
-    rm -f ${NGINX_PURGE_VERSION}.tar.gz
-    rm -f ${NGINX_PHP_VERSION}.tar.gz
-    rm -f ${NGINX_ACCOUNTING_VERSION}.tar.gz
-    rm -f ${NGINX_PCRE_VERSION}.tar.gz
-    
-	# wget ${URL}/soft/nginx/${NGINX_VERSION}.tar.gz
-	#wget ${URL}/soft/nginx/${NGINX_REDIS_VERSION}.tar.gz
-	# wget ${URL}/soft/nginx/${NGINX_REDIS2_VERSION}.tar.gz
-	# wget ${URL}/soft/nginx/${NGINX_SRCACHE_VERSION}.tar.gz
-	# wget ${URL}/soft/nginx/${NGINX_DEVEL_VERSION}.tar.gz
-	# wget ${URL}/soft/nginx/${NGINX_MISC_VERSION}.tar.gz
-	# wget ${URL}/soft/nginx/${NGINX_SLOWFS_VERSION}.tar.gz
-	#  wget ${URL}/soft/nginx/${NGINX_PHP_VERSION}.tar.gz
-    #wget ${URL}/soft/nginx/${NGINX_ACCOUNTING_VERSION}.tar.gz
-    #wget ${URL}/soft/nginx/${NGINX_PCRE_VERSION}.tar.gz
-    
-    cp /mnt/soft/work/nginx/${NGINX_VERSION}.tar.gz                 .
-	cp /mnt/soft/work/nginx/${NGINX_REDIS_VERSION}.tar.gz           .
-	cp /mnt/soft/work/nginx/${NGINX_REDIS2_VERSION}.tar.gz          .
-	cp /mnt/soft/work/nginx/${NGINX_SRCACHE_VERSION}.tar.gz         .
-	cp /mnt/soft/work/nginx/${NGINX_DEVEL_VERSION}.tar.gz           .
-	cp /mnt/soft/work/nginx/${NGINX_MISC_VERSION}.tar.gz            .
-	cp /mnt/soft/work/nginx/${NGINX_SLOWFS_VERSION}.tar.gz          .
-	cp /mnt/soft/work/nginx/${NGINX_PHP_VERSION}.tar.gz             .
-    cp /mnt/soft/work/nginx/${NGINX_ACCOUNTING_VERSION}.tar.gz      .
-    cp /mnt/soft/work/nginx/${NGINX_PCRE_VERSION}.tar.gz            .
-	 
-    
-    rm -rf ${NGINX_VERSION}
-    rm -rf ${APP_HOME}/${NGINX_VERSION}
-    tar -zxvf ${NGINX_VERSION}.tar.gz
-    
-    rm -rf ${NGINX_REDIS_VERSION}
-    tar zxvf ${NGINX_REDIS_VERSION}.tar.gz
-    
-    rm -rf ${NGINX_REDIS2_VERSION}
-    tar zxvf ${NGINX_REDIS2_VERSION}.tar.gz
-    
-    rm -rf ${NGINX_SRCACHE_VERSION}
-    tar zxvf ${NGINX_SRCACHE_VERSION}.tar.gz
-    
-    rm -rf ${NGINX_DEVEL_VERSION}
-    tar zxvf ${NGINX_DEVEL_VERSION}.tar.gz
-    
-    rm -rf ${NGINX_MISC_VERSION}
-    tar zxvf ${NGINX_MISC_VERSION}.tar.gz
-    
-    rm -rf ${NGINX_SLOWFS_VERSION}
-    tar zxvf ${NGINX_SLOWFS_VERSION}.tar.gz
-    
-    rm -rf ${NGINX_PURGE_VERSION}
-    https://github.com/FRiCKLE/ngx_cache_purge.git
-    
-    
-    rm -rf ${NGINX_PHP_VERSION}
-    tar zxvf ${NGINX_PHP_VERSION}.tar.gz
-    
-    rm -rf ${NGINX_ACCOUNTING_VERSION}
-    tar zxvf ${NGINX_ACCOUNTING_VERSION}.tar.gz
-    
-    rm -rf ${NGINX_PCRE_VERSION}
-    tar -zxvf ${NGINX_PCRE_VERSION}.tar.gz
-    
-    rm -rf ${NGINX_ECHO_VERSION}
-    git clone git://github.com/agentzh/echo-nginx-module.git
-    
-    rm -rf ${NGINX_CONSISTENT_HASH_VERSION}
-    git clone https://github.com/replay/ngx_http_consistent_hash.git
+#http://download.redis.io/releases/redis-4.0.10.tar.gz
+  cd /home/web/local
+  wget http://download.redis.io/releases/redis-6.2.6.tar.gz
+  tar -zxvf redis-6.2.6.tar.gz
+  make
+  cp ../../soft/myconf/redis/redis.conf  .
+  ./redis-server ../redis.conf  &
 
-	
-    cd ${NGINX_VERSION}
-	./configure --prefix=${APP_HOME}/${NGINX_VERSION} --with-http_stub_status_module --with-http_ssl_module --with-http_gzip_static_module \
-	--with-pcre=../${NGINX_PCRE_VERSION}
-	--add-module=../${NGINX_REDIS_VERSION} --add-module=../${NGINX_REDIS2_VERSION} --add-module=../${NGINX_SRCACHE_VERSION} \
-	--add-module=../${NGINX_DEVEL_VERSION} --add-module=../${NGINX_PURGE_VERSION} --add-module=../${NGINX_SLOWFS_VERSION=} \
-	--add-module=../${NGINX_PHP_VERSION} --add-module=../${NGINX_ACCOUNTING_VERSION} --add-module=../${NGINX_CONSISTENT_VERSION}  \
-	--add-module=../${NGINX_CONSISTENT_VERSION} --add-module=../${NGINX_ECHO_VERSION}
-	make -j4
-    make install
-    rm -rf ${APP_HOME}/nginx
-    ln -s ${APP_HOME}/${NGINX_VERSION} ${APP_HOME}/nginx
-    rm -rf ${APP_HOME}/nginx/html
-    cd ${shelldir}
-    rm ${APP_HOME}/nginx/conf/nginx.conf
-    cp ../nginx/conf/nginx.conf ${APP_HOME}/nginx/conf/nginx.conf
-    cp ../nginx/server ${APP_HOME}/nginx/conf/ -r
-    echo "mkdir -p /home/web/var/log/  /home/web/var/run/"
 
 }
+
 strongswan_android() 
 {
 	#https://wiki.strongswan.org/issues/652
@@ -434,14 +203,10 @@ if [ $# != 0 ]; then
     for arg in $*; do
         case "$arg" in
             jdk)            setup_jdk;;
-            sdk)          setup_sdk;;
-			ndk)          setup_ndk;;
-			nginx)          setup_nginx;;
 			redis)          setup_redis;;
 			daloradius)         setup_daloradius;;
 			php)         setup_php;;
 			maven)         setup_maven;;
-			android)      strongswan_android;;
 			all)          setup_all;;
         esac
     done

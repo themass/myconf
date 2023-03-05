ae72b4155d3a24b34b33e6b0a26d37541f40b29a
liguoqing06 <liguoqing06@kuaishou.com>
2023-03-06 00:33:31 +0800

    img

--- python/fetch/a7xi/video.py
+++ python/fetch/a7xi/video.py
@@ -100,7 +100,7 @@ class VideoParse(BaseParse):
                             match = videoApi.search(text)
                             if match!=None:
                                 videoUrl =match.group(1)
-                                return "%s%s%s"%("http",videoUrl,'m3u8')
+                                return "%s%s%s"%("http",videoUrl,'.m3u8')
                 print '没找到mp4',url
             return None
         except Exception as e:

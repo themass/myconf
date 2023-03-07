4ea6a35361f9c8bd76d9f7c37ec5cf9ac8e86f88
liguoqing06 <liguoqing06@kuaishou.com>
2023-03-07 09:39:49 +0800

    img

--- python/fetch/xcvods/video.py
+++ python/fetch/xcvods/video.py
@@ -87,7 +87,7 @@ class VideoParse(BaseParse):
         try:
             match = videoId.search(url)
             if match!=None:
-                Id= match.group(2)
+                Id= match.group(1)
                 url  = '/vod-play-id-%s-src-1-num-1.html'%(Id)
                 soup = self.fetchUrl(url, header)
                 div = soup.first('div',{'class':'macplus-player__video embed-responsive embed-responsive-16by9'})

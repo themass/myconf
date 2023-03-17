18cfb14df7a2763d92611b1eb8199102df6ca788
liguoqing06 <liguoqing06@kuaishou.com>
2023-03-17 19:05:49 +0800

    video

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -7,11 +7,13 @@ import jiu667_vip
 import nyg6_vip
 import xcvods_vip
 import xx69_vip
+import hanime_vip
 
 if __name__ == '__main__':
-    # a7xi_vip.parseVideo()
-    # hsex_vip.parseVideo()
-    # jiu667_vip.parseVideo()
-    # nyg6_vip.parseVideo()
+    a7xi_vip.parseVideo()
+    hsex_vip.parseVideo()
+    jiu667_vip.parseVideo()
+    nyg6_vip.parseVideo()
     xcvods_vip.parseVideo()
     xx69_vip.parseVideo()
+    hanime_vip.parseVideo()
--- python/fetch/profile.py
+++ python/fetch/profile.py
@@ -1,5 +1,5 @@
 #!/usr/bin python
 # -*- coding: utf-8 -*-
-maxVideoPage=180
+maxVideoPage=7
 maxImgPage = 100
 maxTextPage=5

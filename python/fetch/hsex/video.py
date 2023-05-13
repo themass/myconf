180c47c28f15c03be12f781a0159dadb1654da3b
liguoqing06 <liguoqing06@kuaishou.com>
2023-05-13 15:03:46 +0800

    1

--- python/fetch/hsex/video.py
+++ python/fetch/hsex/video.py
@@ -26,7 +26,7 @@ class VideoParse(BaseParse):
         dbVPN.close()
         for item in chs:
             url= item['url']
-            for i in range(11, maxVideoPage):
+            for i in range(13, maxVideoPage):
                 con = self.videoParse(item['channel'], item['channelType'],'%s%s%s'%(url.replace('1.htm',''),i,'.htm'))
                 if con==False:
                     print '没有数据了啊-======页数',i,'---',item['name'],item['url']

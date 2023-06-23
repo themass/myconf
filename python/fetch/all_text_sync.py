3f21a69c342ac23bbce95ffa0ec557401a73aee6
liguoqing06 <liguoqing06@kuaishou.com>
2023-06-23 15:48:42 +0800

    img

--- python/fetch/all_text_sync.py
+++ python/fetch/all_text_sync.py
@@ -3,32 +3,16 @@
 from common.envmod import *
 from fetch.不能用 import g6858_vip, sp878_vip, kedouwo_vip, kpd36_vip, ddd804_vip, bx88222_vip
 from fetch import novel941_vip
+from fetch import noval1024_vip
 
 
 def pase1():
-#     novel941_vip.parseText()
-#     ddd804_vip.parsetext65aeae()
-    
-    bx88222_vip.parseText()
-    sp878_vip.parseText()
-    kpd36_vip.parseText()
-def pase2():
-    
-#     se8_vip.startWork()
-#     se8_vip.parseText()
-    novel941_vip.parseText2()
-    ####mayi01_vip.parseText()
-def pase3():
-    g6858_vip.parseText()
-    ddd804_vip.parsetextddd804()
-    kedouwo_vip.parseText12()
+    novel941_vip.parseText()
+    noval1024_vip.parseText()
+    noval1024_vip.parseText2()
+
+
 if __name__ == '__main__':
-    val = argsMap.get("-p",0)
-    if int(val)==1:
-        pase1()
-    elif int(val)==2:
-        pase2()
-    elif int(val)==3:
-        pase3()
+    pase1()
     
     

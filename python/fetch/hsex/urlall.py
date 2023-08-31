632a6e68c5061f5ac14f339ace91d27d8e7c9b13
liguoqing06 <liguoqing06@kuaishou.com>
2023-08-31 10:50:02 +0800

    img

--- python/fetch/hsex/urlall.py
+++ python/fetch/hsex/urlall.py
@@ -1,5 +1,6 @@
-urls = ['search.htm?search=肛交&sort=new',
-        'search.htm?search=按摩&sort=new',
-        'search.htm?search=单男&sort=new',
-        'search.htm?search=电话&sort=new',
-        'search.htm?search=叫声&sort=new']
+urls = ['search.htm?search=%E8%82%9B%E4%BA%A4&sort=new',
+        'search.htm?search=%E6%8C%89%E6%91%A9&sort=new',
+        'search.htm?search=%E5%8D%95%E7%94%B7&sort=new',
+        'search.htm?search=%E7%94%B5%E8%AF%9D&sort=new',
+        'search.htm?search=%E5%8F%AB%E5%A3%B0&sort=new',
+        'search.htm?search=%E5%91%BB%E5%90%9F&sort=new']

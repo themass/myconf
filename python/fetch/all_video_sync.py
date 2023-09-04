dd02ea11e1f46bbc7cf678706fe74bb735659a07
liguoqing06 <liguoqing06@kuaishou.com>
2023-09-04 20:37:02 +0800

    img

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -15,7 +15,7 @@ import tianlalu_vip
 if __name__ == '__main__':
     # hsex_vip.parseVideo()
     # xx69_vip.parseVideo(1, profile.maxVideoPage)
-    xx69_vip.parseVideo2(1, profile.maxVideoPage)
+    # xx69_vip.parseVideo2(1, profile.maxVideoPage)
     nyg6_vip.parseVideo()
     jiu667_vip.parseVideo()
     tianlalu_vip.parseVideo()

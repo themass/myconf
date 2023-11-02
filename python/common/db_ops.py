ca18fdb419b3064454f79092547446398d70218d
liguoqing06 <liguoqing06@kuaishou.com>
2023-11-02 15:30:19 +0800

    img

--- python/common/db_ops.py
+++ python/common/db_ops.py
@@ -155,7 +155,7 @@ class DbOps(object):
         return self.conn.execute(
             "insert ignore into  videoitems_1 (name,url,channel,pic,updateTime,path,videoType,baseurl,sortType,channelType) values ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"
             % (
-                obj.get("name").replace("'",""), obj.get("url"), obj.get("channel").replace(".com",'-'), obj.get("pic"), obj.get("updateTime"), obj.get("path"), videoType,baseUrl,sortType,channelType))
+                obj.get("name").replace("'",""), obj.get("url"), obj.get("channel").replace(".com",'-'), obj.get("pic"), obj.get("updateTime"), obj.get("path"), videoType,obj.get("baseurl"),sortType,channelType))
 
 
     def inertVideoUser(self, obj):

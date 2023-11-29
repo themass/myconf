26acc03a410b8276dcfd01bcc17c3a9717e3d856
liguoqing06 <liguoqing06@kuaishou.com>
2023-11-29 16:24:53 +0800

    img

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -15,12 +15,11 @@ import md51_vip
 import rqsxbyc_vip
 
 if __name__ == '__main__':
-    hsex_vip.parseVideo()
-    hsck_vip.parseVideo()
-    md51_vip.parseVideo()
+    # hsex_vip.parseVideo()
+    # hsck_vip.parseVideo()
+    # md51_vip.parseVideo()
 
-    # nyg6_vip.parseVideo()
-    jiu667_vip.parseVideo()
+    # jiu667_vip.parseVideo()
     tianlalu_vip.parseVideo()
     xx69_vip.parseVideo(1, profile.maxVideoPage)
     xx69_vip.parseVideo2(1, profile.maxVideoPage)
--- python/fetch/profile.py
+++ python/fetch/profile.py
@@ -1,5 +1,5 @@
 #!/usr/bin python
 # -*- coding: utf-8 -*-
-maxVideoPage=300
+maxVideoPage=30
 maxImgPage = 500
 maxTextPage=300

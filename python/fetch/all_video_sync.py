f6db7c388012ab115f075db2c337e43ad4de2903
liguoqing <liguoqing19861028@163.com>
2024-06-22 00:38:53 +0800

    11

--- python/fetch/all_video_sync.py
+++ python/fetch/all_video_sync.py
@@ -1,18 +1,12 @@
 #!/usr/bin python
 # -*- coding: utf-8 -*-
-from common.envmod import *
-import rqsxbyc_vip
 import hsex_vip
 import jiu667_vip
 import xcvods_vip
 import xx69_vip
-import hanime_vip
 import profile
-import  ip38_vip
-import tianlalu_vip
 import hsck_vip
 import md51_vip
-import rqsxbyc_vip
 
 if __name__ == '__main__':
     hsex_vip.parseVideo()
@@ -20,13 +14,8 @@ if __name__ == '__main__':
     md51_vip.parseVideo()
 
     jiu667_vip.parseVideo()
-    tianlalu_vip.parseVideo()
     xx69_vip.parseVideo(1, profile.maxVideoPage)
     xx69_vip.parseVideo2(1, profile.maxVideoPage)
 
-
-    rqsxbyc_vip.parseVideo()
     xcvods_vip.parseVideo()
-    hanime_vip.parseVideo()
-    ip38_vip.parseVideo()
 

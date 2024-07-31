0ed1a48b1523334ff266b93b28491aaffb90ad0c
liguoqing <liguoqing19861028@163.com>
2024-08-01 01:56:11 +0800

    11

--- python/fetch/missav/baseparse.py
+++ python/fetch/missav/baseparse.py
@@ -20,7 +20,13 @@ baseurl = "https://missav.com/"
 header = {'User-Agent':
           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
           'Cookie':'user_uuid=07ca7f21-0a08-42ff-b963-8236cd0e82e2; _ga=GA1.1.600459745.1722393954; cf_clearance=e9BqL9LQZHRlngCxmOtMXdxs6YQgIHE0NyC0CYtIiFw-1722446371-1.0.1.1-NYR3NRfal7dj9cbP6f_5334MQ1XyWK4UWS4_ngf3i_GSEyLWYJoM_k6.L68Uw8IdgPwfYXOSoBWPFz1fX9a0rw; dom3ic8zudi28v8lr6fgphwffqoz0j6c=694fe64e-46e5-4b9b-bfdc-6c8ea530e9e7%3A2%3A1; sb_main_62bdca270715b3b43fbac98597c038f1=1; sb_count_62bdca270715b3b43fbac98597c038f1=6; sb_onpage_62bdca270715b3b43fbac98597c038f1=0; XSRF-TOKEN=eyJpdiI6IjNDSUtPU1ZWREEwM3IzVlZGdG5oZ3c9PSIsInZhbHVlIjoiOVV6RmpTcnpHaDc5TVg2dmN4UUxBaE1PbWdwS1FQOWdQMnNKNkEvNkJ6OUdXRml2UkY2Mkp0MmwvM2ZYSU0wWnd5aHhpNDRreEZDS2dEZTk5cGllUWYxS1ZINTl5Uk1KYkwxRnhqRk5hYkNOWjRhRW5WV2xLZnhUZms3cVhjN3giLCJtYWMiOiI4OWZkMGFiY2RlZDc2MGMxZGFjZGJmNGRmN2U5NWIxYTczZWI1NDM2NzNiY2RhM2NhMTJiMmQwMjc1OThlNjgzIiwidGFnIjoiIn0%3D; missav_session=eyJpdiI6IkNsOWg4Z29yTTBQUVhmaFFYNm9yZUE9PSIsInZhbHVlIjoiUWQ3L1FDTWRHVUVSMmowL1FSK1lXYS9GeWdKb09hZFJNbktYR21JWHYzanVDVktJK2U0Vk1tencwV0tnNjlQVStXZWNZTUlIbzdxVUxyRTcvUTRGT3B4SkdXN1FFRXN5dWNSV24zZXRQMWo4STYxVUp3Z1dqMzFmTU01c284dVYiLCJtYWMiOiI1OGVkYThmN2UyOTZlYTgxYjA0MWEwNzI3MTVhMGQwNjQwMDNlMjY1OGM4YjVmODU4OTUyYTZkZjYyNmI2NDIzIiwidGFnIjoiIn0%3D; capfrMu0V6qRsn6rSi6pdY5SFUzwjgYmk3rDOUbw=eyJpdiI6InhCQ1UwbG1LRVROYXAvd3lBMHU5TUE9PSIsInZhbHVlIjoiVXRFTGY5cVQrc21sSC9CYUR6dHdsSWd1aEdkUGtWdWZidTB2enhXZFNwQlJKczQwYkIyNnZRVjliQmIwa1RMbktRTFVIdXdDSFprOVBGcG9EaWo4ZXhoZTFaZlRUcmYwOTdoK0NIQzdGV0oyckNiY1ROalVYZ1dyMzdOMVUrcVhxeE45WVFOaFVoT25VMEFRNGJXMS95L2tpT1c2b2RsSDQrS3ZncWx0YVNwODFCWFNrcCtDanYvbDdkbVVIWFJVUE1XTlZvaytzQTdlSnQvSnRaa3Vxa2xaeFIrNzJMaGJ0WFRpbWVwelF0Nkc4ZnhoMnNWSnJNZFVoakY0U3NTTFlSTjNkdTZzZzdmZzg0dXlrSlJEZGh4OUNxTXpCS2VDZUtRMTdYd0VWNWIyWGFXQmFQZ0pNRXU2U3FYS29ReFM1UTF5dGY5UlVTZVJNRmx4OVZVdUQ3RW9hOHZoamhLQnNsNStIakgyVGlYd01udTFpUUFnb05zTGllRCtycDJ1RkpzK1RRdWZJWVR6cjBSejU5Z25Qdz09IiwibWFjIjoiZmE5ZTQxOWRkYTM0OTBiZWVmNmM5YTA3YzJjMTQ1YzY1NTMwZjM3OTNjYzNhNWI1MjA0NWEwMGUwNTgyMDRlMCIsInRhZyI6IiJ9; sb_page_62bdca270715b3b43fbac98597c038f1=21; _ga_Z3V6T9VBM6=GS1.1.1722446370.3.1.1722448027.0.0.0'
-          ,"Referer": baseurl}
+          ,"Referer": baseurl,
+          "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+          "accept-language":"zh-CN,zh;q=0.9",
+          "cache-control":"no-cache",
+          "sec-ch-ua":'"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+          "sec-ch-ua-platform":'"macOS"',
+          "upgrade-insecure-requests":"1"}
 maxCount = 3
 regVideo = re.compile(r"http(.*?)m3u8")
 namereg = re.compile(r"(&#[0-9]*;)+")

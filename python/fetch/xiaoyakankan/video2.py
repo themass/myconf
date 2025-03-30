224e6f376c33a6d9f8864250a774b0188a9425c3
liguoqing <liguoqing19861028@163.com>
2025-03-30 09:14:22 +0800

    11

---
+++ python/fetch/xiaoyakankan/video2.py
@@ -0,0 +1,117 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+from baseparse import *
+from urlparse import urlparse
+from common import common
+from fetch.profile import *
+from urllib import unquote
+import sys,time
+reload(sys)
+# 
+sys.setdefaultencoding('utf8')
+
+class VideoParse(BaseParse):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        chs = self.videoChannel()
+        for item in chs:
+            ops.inertVideoChannel(item)
+        print 'xiaoyakankan video -- channel ok;,len=',len(chs)
+        dbVPN.commit()
+        dbVPN.close()
+        for item in chs:
+            url= item['url']
+            for i in range(1, maxVideoPage):
+                con = self.videoParse(item['channel'], item['channelType'],'%s%s%s'%(url.replace('.html','-'),i,'.html'))
+                if con==False:
+                    print '没有数据了啊-======页数',i,'---',item['name'],item['url']
+                    break
+                print '解析完成 ', item['channel'], ' ---', i, '页'
+    def videoChannel(self):
+        channelList = []
+        ahrefs = self.header2()
+        for ahref in ahrefs:
+            obj={}
+            obj['name']=ahref.text
+            obj['url']=ahref.get('href')
+            obj['baseurl']=baseurl
+            obj['updateTime']=datetime.datetime.now()
+            obj['pic']=''
+            obj['rate']=1.2
+            obj['channel']=ahref.text
+            obj['showType']=3
+            obj['channelType']='movie'
+            channelList.append(obj)
+#         channelList.reverse()
+        return  channelList
+    def videoParse(self, channel, channelType, url):
+        dataList = []
+        soup = self.fetchUrl(url)
+        div = soup.first('div',{"class":"gm-list"})
+        if div!=None:
+            divs = div.findAll("div",{"class":"item"})
+            if len(divs)==0:
+                return False
+            for item in divs:
+                ahref = item.first('a')
+                if ahref != None:
+                    obj = {}
+                    mp4Url = self.parseDomVideo(ahref.get("href"))
+                    if mp4Url == None:
+                        print '没有mp4 文件:', ahref.get("href")
+                        continue
+                    obj['url'] = mp4Url
+                    obj['pic'] = '%s%s'%("https:", item.first('img').get('data-src'))
+                    obj['name'] = item.first('img').get("alt")
+
+                    obj['path'] = 'xiaoya'+ahref.get("href")
+                    obj['updateTime'] = datetime.datetime.now()
+                    obj['channel'] = channel
+                    obj['baseurl'] = baseurl+ahref.get("href")
+                    print obj['name'],obj['url'],obj['pic'],obj['baseurl']
+                    dataList.append(obj)
+        dbVPN = db.DbVPN()
+        ops = db_ops.DbOps(dbVPN)
+        for i in range(1, 3):
+            try:
+                for obj in dataList:
+                    ops.inertVideo(obj,"normal",baseurl,channelType)
+                break
+            except Exception as e:
+                print common.format_exception(e)
+        print 'xiaoyakankan video --解析完毕 ; channel =', channel, '; len=', len(dataList), url
+        dbVPN.commit()
+        dbVPN.close()
+        if len(dataList) ==0:
+            return False
+        return True
+    def parseDomVideo(self, url):
+        try:
+            soup = self.fetchUrl(url)
+            scripts = soup.findAll("script")
+            for script in scripts:
+                text = unquote(script.text).replace('\/','/')
+                texts = text.split(",")
+                for item in texts:
+                    if item.count("jpzy01.com") !=0:
+                        continue
+                    match = regVideo.search(item)
+                    if match!=None:
+                        return 'http'+match.group(1)+'.m3u8'
+                for item in texts:
+                    match = regVideo.search(item)
+                    if match!=None:
+                        return 'http'+match.group(1)+'.m3u8'
+            print '没找到mp4'
+            return None
+        except Exception as e:
+            print common.format_exception(e)
+            return None
+
+def videoParse(queue):
+    queue.put(VideoParse())
